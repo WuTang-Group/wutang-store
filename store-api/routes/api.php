157fa2ceec5c9ad3df8ba4d49cf749a852b7fa66
GabbyMrH <932585440@qq.com>
2020-07-07 16:43:22 +0800

    「Route」Api路由组设置拆分，只引入单入口

--- store-api/routes/api.php
+++ store-api/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+/**
+ * Api路由组
+ */
+Route::group(['namespace' => 'Api'], function () {
+    require base_path('routes/api/index.php');
 });

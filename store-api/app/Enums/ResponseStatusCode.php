cd07c5cff68e1e5bf0fdf9bc40919107fbf99759
GabbyMrH <932585440@qq.com>
2020-07-07 15:05:23 +0800

    「Enum」返回状态码修正

--- store-api/app/Enums/ResponseStatusCode.php
+++ store-api/app/Enums/ResponseStatusCode.php
@@ -12,10 +12,10 @@ final class ResponseStatusCode extends Enum
 {
     const ParamError =   10001;       //参数有误
     const DataError =   10003;        //数据有误
-    const QuerySuccess = 20001;       //操作成功
-    const QueryDeny = 40001;          //禁止访问
-    const QueryWithoutAuth = 40003;   //权限不足
+    const RequestSuccess = 20001;       //操作成功
+    const RequestDeny = 40001;          //禁止访问
+    const RequestWithoutAuth = 40003;   //权限不足
     const QueryVoid = 40004;          //查询为空
-    const QueryExpired = 40005;       //请求过期
-    const QueryFails = 50001;         //操作失败
+    const TokenExpired = 40005;       //请求过期
+    const RequestFails = 50001;         //操作失败
 }

e3f35c5a3bf5a65c8e9e6149d1773357bdacc463
宁康 <luckynk@163.com>
2020-07-22 12:43:31 +0800

    - 全局注册权限指令 - 删除生产环境mock服务

--- store-admin/src/main.js
+++ store-admin/src/main.js
@@ -20,18 +20,10 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
-}
+import permission from './directive/permission'
+
+// 注册权限检测指令
+Vue.directive('permission', permission)
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size

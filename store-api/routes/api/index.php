0a23acad6f60699bf12450994c909f57ee82256f
GabbyMrH <gabbymrh@gmail.com>
2020-07-22 21:58:15 +0800

    「Route|Api」路由增加节流组及其他路由

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -1,16 +1,39 @@
 <?php
-Route::group(['middleware' => 'api'], function () {
-    // token无效或失效,针对处理该问题闭包路由
-    Route::get('/unauthorized', function () {
-        return response()->json(\App\Helpers\ResponseData::tokenExpired());
-    })->name('login');
-    // 状态码说明
-    Route::get('response_code','ResponseCodeController@index')->name('response_code.index');
-    /**
-     * auth route
-     */
-    Route::post('auth/login', 'AuthController@login')->name('auth.login');
-    Route::delete('auth/logout', 'AuthController@logout')->name('auth.logout');
-    Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
-    Route::get('auth/me', 'AuthController@me')->name('auth.me');
+
+/**
+ * 无受限路由组
+ */
+// 状态码说明
+Route::get('response_code', 'ResponseCodeController@index')->name('response_code.index');
+
+/**
+ * sign节流路由组
+ */
+Route::middleware('throttle:' . config('api.rate_limits.sign'))
+    ->group(function () {
+        // 图片验证码
+        Route::post('captchas', 'CaptchaController@store')->name('captchas.store');
+        // 用户注册
+        Route::post('auth/register', 'AuthController@register')->name('auth.register');
+    });
+
+/**
+ * access节流路由组
+ */
+Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
+    Route::group(['middleware' => 'api'], function () {
+
+        // token无效或失效,针对处理该问题闭包路由
+        Route::get('/unauthorized', function () {
+            return response()->json(\App\Helpers\ResponseData::tokenExpired());
+        })->name('login');
+
+        /**
+         * auth route
+         */
+        Route::post('auth/login', 'AuthController@login')->name('auth.login');
+        Route::delete('auth/logout', 'AuthController@logout')->name('auth.logout');
+        Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
+        Route::get('auth/me', 'AuthController@me')->name('auth.me');
+    });
 });

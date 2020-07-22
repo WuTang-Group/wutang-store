0c2b1701e9feeee4a6cdce877c65da046b057108
GabbyMrH <gabbymrh@gmail.com>
2020-07-22 21:59:36 +0800

    「Database|migration」个人资料迁移表

---
+++ store-api/database/migrations/2020_07_22_074707_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable()->comment('关联user表');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('real_name')->nullable()->comment('真实姓名');
+            $table->integer('sex')->default(1)->comment('性别,1男,0女,-1其他');
+            $table->timestamp('birthday')->nullable()->comment('出生年月日');
+            $table->string('age')->nullable()->comment('年龄');
+            $table->string('address')->nullable()->comment('住址');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}

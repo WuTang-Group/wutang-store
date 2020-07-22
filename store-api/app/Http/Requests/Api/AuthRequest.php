6c6b67a99802c1f1f81eb344b312b0a0d7ee7a1f
GabbyMrH <gabbymrh@gmail.com>
2020-07-22 10:12:27 +0800

    「Reqest|Api」Auth验证器表单验证

---
+++ store-api/app/Http/Requests/Api/AuthRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+
+use App\Http\Requests\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username'=>'required|string',
+            'password'=>'required|alpha_dash|min:6',
+        ];
+    }
+}

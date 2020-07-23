6d0f224b2c1a581dcb7b44555387e51cf39513c1
GabbyMrH <gabbymrh@gmail.com>
2020-07-23 15:52:26 +0800

    「Controller|Api」验证码控制器

--- store-api/app/Http/Controllers/Api/CaptchaController.php
+++ store-api/app/Http/Controllers/Api/CaptchaController.php
@@ -14,16 +14,15 @@ class CaptchaController extends Controller
      * Generate captcha code and save to cache
      * 生成验证码并写入缓存
      * @queryParam username required 用户名
-     * @param CaptchaRequest $request
      * @param CaptchaBuilder $captchaBuilder
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(CaptchaRequest $request, CaptchaBuilder $captchaBuilder)
+    public function store(CaptchaBuilder $captchaBuilder)
     {
         $key = 'captcha-'.Str::random(15);
         $captcha = $captchaBuilder->build();
-        $expiredAt = now()->addMinutes(3);
-        \Cache::put($key, ['username' => $request->username,'code'=>$captcha,$expiredAt]);
+        $expiredAt = now()->addMinutes(3);  // 3分钟有效期
+        \Cache::put($key, ['code'=>strtolower($captcha->getPhrase()),$expiredAt]);
 
         $result = [
             'captcha_key' => $key,

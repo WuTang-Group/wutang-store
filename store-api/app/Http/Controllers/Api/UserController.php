54c192753a3f039c55570f692e42bcdd5d30facd
GabbyMrH <gabbymrh@gmail.com>
2020-07-23 15:55:01 +0800

    「Controller」用户管理控制器

--- store-api/app/Http/Controllers/Api/UserController.php
+++ store-api/app/Http/Controllers/Api/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\InvitationCode;
 
 class UserController extends Controller
 {
-    //
+    public function test(InvitationCode $invitationCode)
+    {
+        $test = $invitationCode::expiredCode()->full()->get();
+        return response()->json(['data'=>$test]);
+    }
 }

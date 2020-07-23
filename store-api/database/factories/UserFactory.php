4d484e90fce0d7f02b8c9a5988ebf321aac6a356
GabbyMrH <gabbymrh@gmail.com>
2020-07-23 17:25:05 +0800

    「Database|factory」User文件修改

--- store-api/database/factories/UserFactory.php
+++ store-api/database/factories/UserFactory.php
@@ -27,6 +27,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'invitation_code' => Str::random(6),
+        'avatar' => 'http://thirdwx.qlogo.cn/mmopen/vi_32/M5SibDXA0k8bbEGpFAZkyNcUCBZicicgY2RNMEMcoJFwubweq7uocgq6EiacaYpNTFswWicSSfTIYOBKZPnLLwnKgqw/132',
         'created_at' => $faker->dateTime,
         'updated_at' => $faker->dateTime
     ];

eefe77af575a2a419eac447d01ff24ce37aa79d6
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 09:14:37 +0800

    「Controller|Api」User控制器查询方法

--- store-api/app/Http/Controllers/Api/UserController.php
+++ store-api/app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseData;
 use App\Http\Controllers\Controller;
+use App\Services\Api\UserService;
 
 class UserController extends Controller
 {
+    private $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Query user list
+     * 查询用户数据列表
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function list()
     {
-
+        $results = $this->service->list();
+        return response()->json(ResponseData::requestSuccess($results));
     }
 }

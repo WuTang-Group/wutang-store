fb0990eff73a957ee5c3aeecd1cc371b350bde29
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 12:03:42 +0800

    「Enum」角色枚举数据

---
+++ store-api/app/Enums/Roles.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * Class Roles
+ * @package App\Enums
+ */
+final class Roles extends Enum
+{
+    const Admin =   'admin';  // 管理员
+}

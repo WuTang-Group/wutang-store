e4ca3f3233f24556c6348aabea7541c6b72a89a0
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 16:57:04 +0800

    「Database|factory」用户资料模型数据工厂

--- store-api/database/factories/UserProfileFactory.php
+++ store-api/database/factories/UserProfileFactory.php
@@ -9,7 +9,7 @@ $factory->define(\App\Models\Profile::class, function (Faker $faker) {
         'user_id' => $faker->randomElement([1,2,3,4,5]),
         'real_name' => $faker->name,
         'sex' => $faker->randomElement([1,0]),
-        'birthday' => $faker->dateTime,
+        'birthday' => $faker->date(),
         'age' => $faker->randomElement([22,23,24,25,26,27,28,29,30]),
         'created_at' => $faker->dateTime,
         'updated_at' => $faker->dateTime

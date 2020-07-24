2211941f3165452bd05d15daa5ccb4c37abfbca2
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 12:01:04 +0800

    「Route|APi」Api路由组更新修复一些问题

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -15,27 +15,37 @@ Route::middleware('throttle:' . config('api.rate_limits.sign'))
         Route::post('captchas', 'CaptchaController@store')->name('captchas.store');
         // 用户注册
         Route::post('auth/register', 'AuthController@register')->name('auth.register');
+        // token无效或失效,针对处理该问题闭包路由
+        Route::get('/unauthorized', function () {
+            return response()->json(\App\Helpers\ResponseData::tokenExpired());
+        })->name('login');
+        // 用户登录
+        Route::post('auth/login', 'AuthController@login')->name('auth.login');
         // 测试路由
-        Route::get('test','UserController@test');
+        Route::get('test', 'UserController@test');
     });
 
 /**
  * access节流路由组
  */
 Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
-    Route::group(['middleware' => 'api'], function () {
-
-        // token无效或失效,针对处理该问题闭包路由
-        Route::get('/unauthorized', function () {
-            return response()->json(\App\Helpers\ResponseData::tokenExpired());
-        })->name('login');
+    /**
+     * 登录后的路由组
+     */
+    Route::middleware('auth:api')->group(function () {
 
         /**
          * auth route
          */
-        Route::post('auth/login', 'AuthController@login')->name('auth.login');
+        // 退出登录
         Route::delete('auth/logout', 'AuthController@logout')->name('auth.logout');
+        // 刷新token
         Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
+        // 获取登录用户信息
         Route::get('auth/me', 'AuthController@me')->name('auth.me');
+        /**
+         * User route
+         */
+        Route::get('address', 'UserAddressController@list')->name('address.list');
     });
 });

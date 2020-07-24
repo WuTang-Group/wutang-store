c51f84bf22931f122ac951890687f7b0604abd9d
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 17:25:15 +0800

    「Model」Profile模型修改

--- store-api/app/Models/Profile.php
+++ store-api/app/Models/Profile.php
@@ -9,6 +9,12 @@ class Profile extends Model
         'user_id', 'real_name', 'sex', 'birthday', 'age'
     ];
 
+    // 从属关联User模型
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    // 格式化性别输出
     public function getSexAttribute()
     {
         return $this->attributes['sex'] === 1 ? '男' : '女';

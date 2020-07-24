82851abfff0b0a61db642b8bcf6c4e2907d0f084
GabbyMrH <gabbymrh@gmail.com>
2020-07-24 16:55:44 +0800

    「Route|Api」新增用户资料相关路由

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -44,11 +44,17 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         // 获取登录用户信息
         Route::get('auth/me', 'AuthController@me')->name('auth.me');
         /**
-         * User route
+         * User address
          */
         Route::get('user_addresses', 'UserAddressController@list')->name('user_addresses.list');
         Route::post('user_addresses','UserAddressController@store')->name('user_addresses.store');
         Route::put('user_addresses/{address_id}','UserAddressController@update')->name('user_addresses.update');
         Route::delete('user_addresses/{address_id}','UserAddressController@destroy')->name('user_addresses.destroy');
+        /**
+         * User profile
+         */
+        Route::get('user_profiles', 'UserProfileController@list')->name('user_profiles.list');
+        Route::put('user_profiles/{profile_id}','UserProfileController@update')->name('user_profiles.update');
+        Route::delete('user_profiles/{profile}','UserProfileController@destroy')->name('user_profiles.destroy');
     });
 });

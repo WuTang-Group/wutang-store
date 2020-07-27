06d7db5efc9e90080920e2a3a558f77a81c1670d
GabbyMrH <gabbymrh@gmail.com>
2020-07-27 20:11:11 +0800

    「Database|migration」套盒产品表

---
+++ store-api/database/migrations/2020_07_24_125634_create_case_ment_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCaseMentProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('case_ment_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('banner_img')->nullable()->comment('banner图');
+            $table->text('banner_text')->nullable()->comment('banner部分文本');
+            $table->text('case_ment_benifit')->nullable()->comment('套盒功效');
+            $table->text('case_ment_advantage')->nullable()->comment('套盒优点');
+            $table->string('main_img')->nullable()->comment('套盒内容介绍主图');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('case_ment_products');
+    }
+}

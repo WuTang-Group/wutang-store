d170f660ae76e0a5b78149968dca59dba27415e3
GabbyMrH <gabbymrh@gmail.com>
2020-07-27 20:11:49 +0800

    「Database|migration」购物车表

---
+++ store-api/database/migrations/2020_07_27_185552_create_shop_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->comment('所属用户id');
+            $table->unsignedBigInteger('product_sku_id')->nullable()->comment('所属sku表id');
+            $table->unsignedInteger('amount')->comment('数量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_carts');
+    }
+}

3cf5605aa6e101a2e6fab3ec7623ed3f8eed4c3e
GabbyMrH <gabbymrh@gmail.com>
2020-07-29 20:33:53 +0800

    「Config」配置增加引入银联支付相关配置

--- store-api/config/laravel-omnipay.php
+++ store-api/config/laravel-omnipay.php
@@ -14,7 +14,18 @@ return [
 				'landingPage'    => '',
 				'headerImageUrl' => ''
 			]
-		]
+		],
+        'unionpay' => [
+            'driver' => 'UnionPay_Express',
+            'options' => [
+                'merId' => '777290058184121',
+                'certPath' => '/Users/gabbymrh/DevelopTools/union-pay-cert/acp_test_sign.pfx',
+                'certPassword' =>'000000',
+                'certDir'=>'/Users/gabbymrh/DevelopTools/union-pay-cert',
+                'returnUrl' => 'http://store.wutang.com/unionpay/return',
+                'notifyUrl' => 'dsadadas'
+            ]
+        ]
 	]
 
 ];

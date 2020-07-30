c1a4d90df04bab8e8cd0f5c593d395fc75479b68
GabbyMrH <gabbymrh@gmail.com>
2020-07-30 19:45:43 +0800

    「Enum」订单号相关状态枚举数据类

---
+++ store-api/app/Enums/OrderStatusCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+
+final class OrderStatusCode extends Enum
+{
+    const StatusPlaced = 1;  // 已下单
+    const StatusPending = 2;  // 已付款
+    const StatusDeliverd = 3;  // 未付款
+    const StatusReceived = -1;  // 付款失败
+}

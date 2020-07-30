525fe095566aedd96f9fcaa99872ecad75dc6147
GabbyMrH <gabbymrh@gmail.com>
2020-07-30 19:47:29 +0800

    「Request|Api」支付网关控制器对应参数验证器

---
+++ store-api/app/Http/Requests/Api/PaymentRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+
+use App\Http\Requests\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->route()->getActionMethod()) {
+            case 'payByUnionpay' :
+            {
+                return [
+                    'no' => 'required',
+                    'product_name' => 'required|string',
+                    'total_amount' => 'required'
+                ];
+            }
+        }
+    }
+}

ae37953e78f4099b753c7b9aeae59bf42874662f
GabbyMrH <gabbymrh@gmail.com>
2020-07-30 19:40:06 +0800

    「Route|Web」调整剔除测试路由

--- store-api/routes/web/index.php
+++ store-api/routes/web/index.php
@@ -2,8 +2,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('test',function(){
-//    $data = ResponseData::paramError('123','456');
-//    return $data;
-    throw new \App\Exceptions\HttpResponseException(ResponseData::tokenExpired('123','456'));
-});

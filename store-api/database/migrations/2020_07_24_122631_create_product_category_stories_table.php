1cd74952791505afc7d5abf2a5234db27d5c7069
GabbyMrH <gabbymrh@gmail.com>
2020-08-03 18:12:39 +0800

    「Database|migration」 * 新增产品分类故事迁移文件

---
+++ store-api/database/migrations/2020_07_24_122631_create_product_category_stories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoryStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category_stories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable()->comment('分类故事名称');
+            $table->string('title_en')->nullable()->comment('英文故事名称');
+            $table->text('description')->nullable()->comment('分类简介');
+            $table->string('description_en')->nullable()->comment('英文分类简介');
+            $table->string('banner')->nullable()->comment('故事banner');
+            $table->string('product_category_id')->nullable()->comment('属于产品类ID');
+            $table->foreign('product_category_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category_stories');
+    }
+}

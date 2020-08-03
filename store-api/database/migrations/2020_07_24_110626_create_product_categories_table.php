266a58292bcccb0a485487c771cea968ac9dfa04
GabbyMrH <gabbymrh@gmail.com>
2020-08-03 18:12:03 +0800

    「Databse|migration」 * 新增产品分类迁移文件

--- store-api/database/migrations/2020_07_24_110626_create_product_categories_table.php
+++ store-api/database/migrations/2020_07_24_110626_create_product_categories_table.php
@@ -16,7 +16,14 @@ class CreateProductCategoriesTable extends Migration
         Schema::create('product_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title')->nullable()->comment('分类名称');
-            $table->string('description')->nullable()->comment('分类简介');
+            $table->string('title_en')->nullable()->comment('英文分类名称');
+            $table->text('describe')->nullable()->comment('分类描述');
+            $table->text('describe_en')->nullable()->comment('英文分类描述');
+            $table->string('banner')->nullable()->comment('分类banner图');
+            $table->text('description')->nullable()->comment('分类简介');
+            $table->text('description_en')->nullable()->comment('英文分类简介');
+            $table->string('img')->nullable()->comment('分类简介图');
+            $table->unsignedBigInteger('parent_id')->nullable()->comment('分类上级');
             $table->timestamps();
         });
     }

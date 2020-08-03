4b629c81c92c3b93c51e02c6b4f3112195e7016b
GabbyMrH <gabbymrh@gmail.com>
2020-08-03 14:52:03 +0800

    「Database|migration」 * 用户表增加密保问题和答案

--- store-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ store-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique()->comment('用户名:即用户账号');
             $table->string('email')->nullable()->unique()->comment('邮箱');
             $table->timestamp('email_verified_at')->nullable()->comment('邮箱验证时间');
+            $table->unsignedBigInteger('password_question_id')->comment('关联密保问题id');
+            $table->string('password_answer')->comment('密保问题答案');
             $table->string('phone')->nullable()->unique()->comment('手机号');
             $table->string('password')->comment('密码');
             $table->rememberToken();
-            $table->string('invitation_code')->nullable()->comment('用户被邀请的邀请码');
             $table->integer('status')->default(1)->comment('账号状态');
             $table->string('avatar')->nullable()->comment('用户头像');
             $table->timestamps();

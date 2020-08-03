7ec42c59bcf6c9de3228bc4013697bb8c46c85c7
GabbyMrH <gabbymrh@gmail.com>
2020-08-03 14:57:02 +0800

    「Controller|Api」 * 验证码控制器移除对应表单验证器

--- store-api/app/Http/Controllers/Api/CaptchaController.php
+++ store-api/app/Http/Controllers/Api/CaptchaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\CaptchaRequest;
 use Gregwar\Captcha\CaptchaBuilder;
 use Illuminate\Support\Str;
 

a052e8c49e9687b34d0a7069f40394b11ccb7035
GabbyMrH <gabbymrh@gmail.com>
2020-08-04 12:01:07 +0800

    「Enum」 * 支付宝支付状态码封装

---
+++ store-api/app/Enums/AlipayCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+
+final class AlipayCode extends Enum
+{
+    const WAIT_BUYER_PAY = 'WAIT_BUYER_PAY';  // 交易创建，等待买家付款
+    const TRADE_CLOSED = 'TRADE_CLOSED';  // 在指定时间段内未支付时关闭的交易;在交易完成全额退款成功时关闭的交易
+    const TRADE_SUCCESS = 'TRADE_SUCCESS';  // 交易成功，且可对该交易做操作，如：多级分润、退款等
+    const TRADE_FINISHED = 'TRADE_FINISHED';  // 交易成功且结束，即不可再做任何操作
+}

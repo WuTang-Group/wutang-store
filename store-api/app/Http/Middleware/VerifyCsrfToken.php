a20872fe302fd7abd9a41d470390a9f1b60a698b
GabbyMrH <gabbymrh@gmail.com>
2020-08-04 12:00:43 +0800

    「Middleware」 * 中间件过滤路由

--- store-api/app/Http/Middleware/VerifyCsrfToken.php
+++ store-api/app/Http/Middleware/VerifyCsrfToken.php
@@ -20,6 +20,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'unionpay/return',
-        'unionpay/notify'
+        'unionpay/notify',
+        'api/alipay/notify'
     ];
 }

fafcc19acf3927fb04b4945e183a5e91b9782cd9
peng0228 <770601900@qq.com>
2020-08-05 18:27:03 +0800

    「Product|Model」 * 修改product模型，限制可修改字段

--- store-api/app/Models/Product.php
+++ store-api/app/Models/Product.php
@@ -5,8 +5,35 @@ namespace App\Models;
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'product_category_id',
+        'product_name',
+        'product_name_en',
+        'thumbnail',
+        'slug',
+        'short_description',
+        'short_description_en',
+        'price',
+        'sale_price',
+        'stock',
+        'seo_title',
+        'seo_keyword',
+        'seo_description',
+        'benefit',
+        'benefit_en',
+        'tech_description',
+        'tech_description_en',
+        'description',
+        'description_en',
+        'usage',
+        'usage_en',
+        'main_image',
+        'main_image_2',
+        'benefit_image',
+        'product_video',
+        'status',
+        'rating',
+        'sold_count',
+        'review_count'
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段

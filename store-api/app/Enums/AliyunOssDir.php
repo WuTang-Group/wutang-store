db0968d157166c33defb6ef88b8819d9a7bee785
GabbyMrH <gabbymrh@gmail.com>
2020-08-06 09:43:01 +0800

    「Enum」 * 新增枚举数据类用于规范化命名阿里云OSS文件夹

---
+++ store-api/app/Enums/AliyunOssDir.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+
+final class AliyunOssDir extends Enum
+{
+    const Product = 'Products';
+    const ProductCategory = 'ProductCategories';
+    const ProductCategoryStory = 'ProductCategoryStories';
+}

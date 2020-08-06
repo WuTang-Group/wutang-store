744ae0f62be7e901460a749ae287fc895113746d
GabbyMrH <gabbymrh@gmail.com>
2020-08-06 20:19:46 +0800

    「Route|Api」 * 路由整合分割Admin

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -42,7 +42,6 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         /**
          * 支付支付路由组
          */
-
         Route::get('alipay/pay','PaymentController@payByAlipay')->name('alipay.payByAlipay');
         Route::get('alipay/return', 'PaymentController@alipayReturn')->name('alipay.alipayReturn');
         Route::post('alipay/notify', 'PaymentController@alipayNotify')->name('alipay.alipayNotify');
@@ -66,68 +65,11 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
         // 获取登录用户信息
         Route::get('auth/me', 'AuthController@me')->name('auth.me');
-        /**
-         * User info
-         */
-        // 获取用户信息
-        Route::get('user', 'UserController@queryList')->name('user.queryList');
-        // 更新用户信息
-        Route::put('user/{username}', 'UserController@update')->name('user.update');
-        // 删除用户
-        Route::delete('user/{username}', 'UserController@destroy')->name('user.destroy');
-        // 修改密码
-        Route::put('user/reset_password/{username}', 'UserController@resetPassword')->name('user.resetPassword');
-        // 获取单个用户信息
-        Route::get('get_user', 'UserController@info')->name('user.info');
-        /**
-         * User address
-         */
-        Route::get('user_addresses', 'UserAddressController@queryList')->name('user_addresses.queryList');
-        Route::post('user_addresses', 'UserAddressController@store')->name('user_addresses.store');
-        Route::put('user_addresses/{address_id}', 'UserAddressController@update')->name('user_addresses.update');
-        Route::delete('user_addresses/{address_id}', 'UserAddressController@destroy')->name('user_addresses.destroy');
-        /**
-         * User profile
-         */
-        Route::get('user_profiles', 'UserProfileController@queryList')->name('user_profiles.queryList');
-        Route::put('user_profiles/{profile_id}', 'UserProfileController@update')->name('user_profiles.update');
-        Route::delete('user_profiles/{profile}', 'UserProfileController@destroy')->name('user_profiles.destroy');
-        /**
-         * invitation Code
-         */
-        Route::get('invitation/code', 'InvitationController@genInvitation');
-        Route::post('invitation/code', 'InvitationController@bindCode');
-        Route::get('invitation/phone', 'InvitationController@get_user');
-        /**
-         * product categories
-         */
-        Route::get('product_categories', 'ProductCategoryController@queryList')->name('product_categories.queryList');
-        Route::post('product_categories', 'ProductCategoryController@store')->name('product_categories.store');
-        Route::post('product_categories/{productCategoriesId}', 'ProductCategoryController@edit')->name('product_categories.edit');
-        Route::delete('product_categories/{productCategoriesId}', 'ProductCategoryController@destroy')->name('product_categories.destroy');
 
         /**
-         * product categories story
+         * Admin相关路由(登录后)
          */
-        Route::get('category_stories', 'CategoryStoryController@queryList')->name('category_stories.queryList');
-        Route::post('category_stories', 'CategoryStoryController@store')->name('category_stories.store');
-        Route::post('category_stories/{CategoriesStoriesId}', 'CategoryStoryController@edit')->name('category_stories.edit');
-        Route::delete('category_stories/{CategoriesStoriesId}', 'CategoryStoryController@destroy')->name('category_stories.destroy');
-
-        /**
-         * product
-         */
-        Route::get('product', 'ProductController@queryList')->name('product.queryList');
-        Route::post('product', 'ProductController@store')->name('product.store');
-        Route::post('product/{productId}', 'ProductController@edit')->name('product.edit');
-        Route::delete('product/{productId}', 'ProductController@destroy')->name('product.destroy');
-        /**
-         * product SKU
-         */
-        Route::get('product_sku', 'ProductSkuController@queryList')->name('product_sku.queryList');
-        Route::post('product_sku', 'ProductSkuController@store')->name('product_sku.store');
-        Route::post('product_sku/{productSkuId}', 'ProductSkuController@edit')->name('product_sku.edit');
-        Route::delete('product_sku/{productSkuId}', 'ProductSkuController@destroy')->name('product_sku.destroy');
+        require base_path('routes/admin/index.php');
     });
 
 });

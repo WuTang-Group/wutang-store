a6f310d7e78d8f7c8258175da9418b1c7ca298e6
GabbyMrH <gabbymrh@gmail.com>
2020-08-07 17:28:55 +0800

    「Enum」 * 修改未付款状态为0

--- store-api/app/Enums/OrderStatusCode.php
+++ store-api/app/Enums/OrderStatusCode.php
@@ -9,6 +9,6 @@ final class OrderStatusCode extends Enum
 {
     const StatusPlaced = 1;  // 已下单
     const StatusPending = 2;  // 已付款
-    const StatusDeliverd = 3;  // 未付款
+    const StatusDeliverd = 0;  // 未付款
     const StatusReceived = -1;  // 付款失败
 }

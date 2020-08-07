d7e09d6c9f25ba9ffd81d33860d0e64a3a5f308a
GabbyMrH <gabbymrh@gmail.com>
2020-08-07 19:15:16 +0800

    「Controller|Api」 * 订单类路由说明

--- store-api/app/Http/Controllers/Api/OrderController.php
+++ store-api/app/Http/Controllers/Api/OrderController.php
@@ -6,7 +6,10 @@ use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\OrderRequest;
 use App\Services\Api\OrderService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class OrderController extends Controller
 {
@@ -16,21 +19,33 @@ class OrderController extends Controller
         $this->service = $service;
     }
 
-    // 订单列表
+    /**
+     * Get order list
+     * 获取订单列表
+     * @return Application|ResponseFactory|Response
+     */
     public function queryList()
     {
         $results = $this->service->queryList();
         return response(ResponseData::requestSuccess($results));
     }
 
-    // 订单详情
+    /**
+     * Get order detail
+     *  获取订单详情
+     * @return Application|ResponseFactory|Response
+     */
     public function getOrderDetail()
     {
         $results = $this->service->getOrderDetail();
         return response(ResponseData::requestSuccess($results));
     }
 
-    // 订单创建发起
+    /**
+     * Request create order(NO USE NOW)
+     *  订单创建发起(当前未启用)
+     * @param OrderRequest $request
+     */
     public function requestStore(OrderRequest $request)
     {
         $results = $this->service->requestStore($request);

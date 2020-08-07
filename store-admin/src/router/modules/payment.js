b8286d0729b3c8ae0f022c7addaa6cad14e60a44
GabbyMrH <gabbymrh@gmail.com>
2020-08-07 16:22:28 +0800

    「AdminAPP」 * 支付管理路由组

---
+++ store-admin/src/router/modules/payment.js
@@ -0,0 +1,13 @@
+import Layout from '@/layout'
+
+const paymentRouter = {
+  path: '/payment',
+  component: Layout,
+  name: 'Payment',
+  meta: {
+    title: '支付管理',
+    icon: 'payment'
+  }
+}
+
+export default paymentRouter

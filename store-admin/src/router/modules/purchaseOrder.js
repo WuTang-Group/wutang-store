2260ec2124601e760b380e547e6918aceaf58f2a
GabbyMrH <gabbymrh@gmail.com>
2020-08-07 16:22:45 +0800

    「AdminAPP」 * 订单管理路由组

---
+++ store-admin/src/router/modules/purchaseOrder.js
@@ -0,0 +1,23 @@
+import Layout from '@/layout'
+
+const orderRouter = {
+  path: '/purchase-order',
+  component: Layout,
+  redirect: '/purchase-order/list',
+  alwaysShow: true,
+  name: 'PurchaseOrder',
+  meta: {
+    title: '订单管理',
+    icon: 'order'
+  },
+  children: [
+    {
+      path: 'list',
+      component: () => import('@/views/table/inline-edit-table'),
+      name: 'PurchaseOrderList',
+      meta: { title: '订单列表' }
+    }
+  ]
+}
+
+export default orderRouter

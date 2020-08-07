e32b632689a8dfb513f4a45807e1971b691b9861
GabbyMrH <gabbymrh@gmail.com>
2020-08-07 16:18:13 +0800

    「Service|Api」 * 购物车服务类增设对应方法(写入数据和读取数据)

---
+++ store-api/app/Services/Api/ShopCartService.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Services\Api;
+
+use App\Models\ShopCartItem;
+use App\Models\User;
+use App\Services\Service;
+use Illuminate\Support\Facades\Log;
+
+class ShopCartService extends Service
+{
+    private $shopCart;
+    public function __construct(ShopCartItem $shopCart)
+    {
+        $this->shopCart = $shopCart;
+    }
+
+    public function queryList()
+    {
+        return User::with(['shopCartItems','shopCartItems.product'])->whereId($this->user()->id)->get();
+    }
+
+    public function store($queries)
+    {
+        try {
+            $this->user()->shopCartItems()->create($queries);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return false;
+        }
+        return $this->queryList();
+    }
+}

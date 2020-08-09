cca234b8b26aa2256d7401a09bcf78a1c73705a0
GabbyMrH <gabbymrh@gmail.com>
2020-08-09 13:14:22 +0800

    「Route|APi」 * API路由整理

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -34,35 +34,9 @@ Route::middleware('throttle:' . config('api.rate_limits.sign'))
  */
 Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
     /**
-     * 登录后的路由组
+     * 无需登录路由组
      */
-    Route::middleware('auth:api')->group(function () {
-        /**
-         * 支付支付路由组
-         */
-        Route::get('alipay/pay','PaymentController@payByAlipay')->name('alipay.payByAlipay');
-        Route::get('alipay/return', 'PaymentController@alipayReturn')->name('alipay.alipayReturn');
-        Route::post('alipay/notify', 'PaymentController@alipayNotify')->name('alipay.alipayNotify');
-
-        /**
-         * 银联支付路由
-         */
-        // 发起支付
-        Route::get('unionpay/pay','PaymentController@payByUnionpay')->name('unionpay.payByUnionpay');
-        // 支付后回调url
-        Route::post('unionpay/return','PaymentController@unionpayReturn')->name('unionpay.unionpayReturn');
-        // 支付后通知url
-        Route::post('unionpay/notify','PaymentController@unionpayNotify')->name('unionpay.unionpayNotify');
-
-        /**
-         * auth route
-         */
-        // 退出登录
-        Route::delete('auth/logout', 'AuthController@logout')->name('auth.logout');
-        // 刷新token
-        Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
-        // 获取登录用户信息
-        Route::get('auth/me', 'AuthController@me')->name('auth.me');
+    Route::group(['middleware' => 'guest'], function () {
         /**
          * Product
          */
@@ -74,16 +48,53 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         Route::get('product/{categoryId}', 'ProductController@getCategoryProduct')->name('product.getCategoryProduct');
         // 新品
         Route::get('product_new', 'ProductController@newProduct')->name('product.newProduct');
+    });
+    /**
+     * 需登录的路由组
+     */
+    Route::middleware('auth:api')->group(function () {
+        /**
+         * 支付支付路由组
+         */
+        Route::get('alipay/pay', 'PaymentController@payByAlipay')->name('alipay.payByAlipay');
+        Route::get('alipay/return', 'PaymentController@alipayReturn')->name('alipay.alipayReturn');
+        Route::post('alipay/notify', 'PaymentController@alipayNotify')->name('alipay.alipayNotify');
+
+        /**
+         * 银联支付路由
+         */
+        // 发起支付
+        Route::get('unionpay/pay', 'PaymentController@payByUnionpay')->name('unionpay.payByUnionpay');
+        // 支付后回调url
+        Route::post('unionpay/return', 'PaymentController@unionpayReturn')->name('unionpay.unionpayReturn');
+        // 支付后通知url
+        Route::post('unionpay/notify', 'PaymentController@unionpayNotify')->name('unionpay.unionpayNotify');
+
+        /**
+         * auth route
+         */
+        // 退出登录
+        Route::delete('auth/logout', 'AuthController@logout')->name('auth.logout');
+        // 刷新token
+        Route::put('auth/refresh', 'AuthController@refresh')->name('auth.refresh');
+        // 获取登录用户信息
+        Route::get('auth/me', 'AuthController@me')->name('auth.me');
+        /**
+         * User profile
+         */
+        Route::get('user_profiles', 'UserProfileController@queryList')->name('user_profiles.queryList');
+        Route::put('user_profiles/{profile_id}', 'UserProfileController@update')->name('user_profiles.update');
+        Route::delete('user_profiles/{profile}', 'UserProfileController@destroy')->name('user_profiles.destroy');
         /**
          * 购物车
          */
         // 结合cookie:若传参数则表示是通过cookie或手动写入数据表并返回最新购物车信息，若不传参数则返回已有购物车表数据信息
-        Route::post('shop_carts','ShopCartController@queryList')->name('shop_carts.queryList');
+        Route::post('shop_carts', 'ShopCartController@queryList')->name('shop_carts.queryList');
         /**
          * 订单
          */
-        Route::get('orders','OrderController@queryList')->name('orders.queryList');
-        Route::get('order_details','OrderController@getOrderDetail')->name('order_details.getOrderDetail');
+        Route::get('orders', 'OrderController@queryList')->name('orders.queryList');
+        Route::get('order_details', 'OrderController@getOrderDetail')->name('order_details.getOrderDetail');
 
     });
 

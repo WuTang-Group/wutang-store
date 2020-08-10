33af4eff54d92339ec03fa4fc478df4b19d97a18
GabbyMrH <gabbymrh@gmail.com>
2020-08-10 15:00:29 +0800

    「Service|Api」 * 产品服务类增加获取 产品详情以slug形式

--- store-api/app/Services/Api/ProductService.php
+++ store-api/app/Services/Api/ProductService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Services\Api;
 
 use App\Enums\ProductStatusCode;
@@ -11,55 +12,45 @@ use Illuminate\Support\Facades\Log;
 class ProductService extends Service
 {
     private $product, $productCategory;
+
     public function __construct(Product $product, ProductCategory $productCategory)
     {
         $this->product = $product;
         $this->productCategory = $productCategory;
     }
 
+    // 获取指定slug的产品详情
+    public function index($queries)
+    {
+        return $this->product->whereSlug($queries)->first();
+    }
+
     // 获取商品类别
-    public function categoryQueryList(){
-        try{
-            return DB::table('product_categories as p1')
-                ->leftJoin('product_categories as p2', 'p1.parent_id', '=', 'p2.id')
-                ->select('p1.*', 'p2.title as parent_title')
-                ->orderBy('id', 'asc')
-                ->get();
-        }catch(\Exception $e){
-            Log::error($e->getMessage());
-            return false;
-        }
+    public function categoryQueryList()
+    {
+        return DB::table('product_categories as p1')
+            ->leftJoin('product_categories as p2', 'p1.parent_id', '=', 'p2.id')
+            ->select('p1.*', 'p2.title as parent_title')
+            ->orderBy('id', 'asc')
+            ->get();
     }
 
     // 所有商品列表
-    public function productQueryList($queries){
-        try{
-            return $this->product->paginate($queries['page_limit']);
-        }catch (\Exception $e){
-            Log::error($e->getMessage());
-            return false;
-        }
+    public function productQueryList($queries)
+    {
+        return $this->product->paginate($queries['page_limit']);
     }
 
     // 获取对应类别下的产品
-    public function getCategoryProduct($categoryId, $queries)
+    public function getCategoryProduct($category_slug, $queries)
     {
-        try{
-            return $this->product->where('product_category_id', $categoryId)->paginate($queries['page_limit']);
-        }catch (\Exception $e){
-            Log::error($e->getMessage());
-            return false;
-        }
+        $requestData = page_limit($queries->all());
+        return $this->product->whereSlug($category_slug)->paginate($requestData['page_limit']);
     }
 
     // 获取新品
-    public function newProduct($queries){
-        try{
-            Log:info($queries);
-            return $this->product->where('status', ProductStatusCode::StatusNew)->paginate($queries['page_limit']);
-        }catch (\Exception $e){
-            Log::error($e->getMessage());
-            return false;
-        }
+    public function newProduct($queries)
+    {
+        return $this->product->whereStatus(ProductStatusCode::StatusNew)->paginate($queries['page_limit']);
     }
 }

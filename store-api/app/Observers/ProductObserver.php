5c7c36f132fac1ea17e326494daca5871dd0a777
GabbyMrH <gabbymrh@gmail.com>
2020-08-10 16:29:44 +0800

    「Observer」 * 产品模型监视器自动翻译slug

---
+++ store-api/app/Observers/ProductObserver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Observers;
+
+// creating, created, updating, updated, saving,
+// saved,  deleting, deleted, restoring, restored
+
+use App\Handlers\SlugTranslateHandler;
+use App\Models\Product;
+
+class ProductObserver
+{
+    public function saving(Product $product)
+    {
+        // XSS 过滤
+        $product->short_description = clean($product->short_description, 'product_short_description');
+
+        if (!$product->slug) {
+            $product->slug = app(SlugTranslateHandler::class)->translate($product->short_description);
+        }
+    }
+}

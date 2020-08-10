2cfea29d5179c859e6536eaf8760f562582a71f9
GabbyMrH <gabbymrh@gmail.com>
2020-08-10 14:59:25 +0800

    「Controller|Api」 * 增加用户地址控制器 * 增加用户地址列表方法

---
+++ store-api/app/Http/Controllers/Api/UserAddressController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Handlers\ResponseData;
+use App\Http\Controllers\Controller;
+use App\Services\Api\UserAddressService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+
+class UserAddressController extends Controller
+{
+    private $service;
+    public function __construct(UserAddressService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @return Application|ResponseFactory|Response
+     */
+    public function index()
+    {
+        $results = $this->service->index();
+        return response(ResponseData::requestSuccess($results));
+    }
+}

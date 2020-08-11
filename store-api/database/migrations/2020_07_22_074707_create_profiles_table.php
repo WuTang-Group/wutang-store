7277b23a26eb4f3bcbea2aa63ff8d330fce859ed
peng0228 <770601900@qq.com>
2020-08-11 15:02:28 +0800

    「Profiles|Model」 * 增加用户信息的地址、手机等相关信息字段

--- store-api/database/migrations/2020_07_22_074707_create_profiles_table.php
+++ store-api/database/migrations/2020_07_22_074707_create_profiles_table.php
@@ -17,10 +17,16 @@ class CreateProfilesTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id')->nullable()->comment('关联user表');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('phone')->nullable()->unique()->comment('联系电话');
             $table->string('real_name')->nullable()->comment('真实姓名');
             $table->integer('sex')->default(1)->comment('性别,1男,0女,-1其他');
             $table->date('birthday')->nullable()->comment('出生年月日');
             $table->string('age')->nullable()->comment('年龄');
+            $table->string('province')->nullable()->comment('省');
+            $table->string('city')->nullable()->comment('市');
+            $table->string('district')->nullable()->comment('区');
+            $table->string('address')->nullable()->comment('详细地址');
+            $table->string('zip')->nullable()->comment('邮编');
             $table->timestamps();
         });
     }

0209f7502b78fc68da0fcda01e025b70f532f3af
GabbyMrH <gabbymrh@gmail.com>
2020-08-11 14:17:26 +0800

    「Seeder」 * 更改表明语义化

--- store-api/database/seeds/DatabaseSeeder.php
+++ store-api/database/seeds/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductTableSeeder::class);
         $this->call(ProductSkuTableSeeder::class);
         $this->call(ProductCategoriesTableSeeder::class);
-        $this->call(CategoryStoryTableSeeder::class);
+        $this->call(ProductCategoryStoriesTableSeeder::class);
         $this->call(ShopCartTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
         $this->call(OrderItemTableSeeder::class);

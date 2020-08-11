54364d7cc6b5bb723a0e3dd02de7a83e28c22c4a
GabbyMrH <gabbymrh@gmail.com>
2020-08-11 15:12:17 +0800

    「Enum」 * 护肤步骤状态说明

--- store-api/app/Enums/SkinCareStepStatus.php
+++ store-api/app/Enums/SkinCareStepStatus.php
@@ -7,6 +7,6 @@ use BenSampo\Enum\Enum;
 
 final class SkinCareStepStatus extends Enum
 {
-    const Recommend = 1;
-    const Optional = 0;
+    const Recommend = 1; // 官方推荐
+    const Optional = 0;  // 用户自选
 }

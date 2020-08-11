6e4adf864ac94bf1af568294c6bedff544181b79
GabbyMrH <gabbymrh@gmail.com>
2020-08-11 15:09:31 +0800

    「Dababase|migration」 * 新增护肤步骤迁移文件(备用)

---
+++ store-api/database/migrations/2020_08_11_143550_create_skin_care_steps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkinCareStepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skin_care_steps', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('step_first')->index()->nullable()->comment('步骤1关联产品');
+            $table->unsignedBigInteger('step_second')->index()->nullable()->comment('步骤2关联产品');
+            $table->unsignedBigInteger('step_third')->index()->nullable()->comment('步骤3关联产品');
+            $table->integer('status')->default(1)->comment('1表示推荐，0表示自选');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skin_care_steps');
+    }
+}

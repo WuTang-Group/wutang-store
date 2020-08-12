827fd890287217b7a55d72d354d4a081693b98fb
GabbyMrH <gabbymrh@gmail.com>
2020-08-12 20:13:52 +0800

    「Service|APi」 * 完善订单下单方法逻辑 * 增加订单查询方法

--- store-api/app/Services/Api/OrderService.php
+++ store-api/app/Services/Api/OrderService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services\Api;
 
+use App\Enums\AlipayCode;
+use App\Enums\AlipayGatewayCode;
+use App\Enums\OrderStatusCode;
+use App\Enums\UnionPayCode;
 use App\Handlers\OrderHandler;
 use App\Models\Order;
 use App\Models\Product;
@@ -9,6 +13,7 @@ use App\Models\UserAddress;
 use App\Services\Service;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Omnipay\Common\Exception\InvalidRequestException;
 
 class OrderService extends Service
 {
@@ -32,40 +37,45 @@ class OrderService extends Service
         // return $this->order->load(['items.product'])->whereUserId($this->user()->id)->get();
     }
 
-    public function requestStore($queries)
+    public function requestCreate($queries)
     {
         $user = $this->user();
         try {
-            $orderRequest = DB::transaction(function () use ($user, $queries){
+            $orderRequest = DB::transaction(function () use ($user, $queries) {
                 $address = UserAddress::find($queries->address_id);
                 // 更新此地址 最后使用时间
                 $address->update(['last_used_at' => now()]);
                 // 创建一个订单
-                $order   = new Order([
+                $order = new Order([
                     'no' => OrderHandler::createOnlyId(),
-                    'address_id'      => $queries->address_id,
-                    'remark'       => $queries->remark,
+                    'address_id' => $queries->address_id,
+                    'remark' => $queries->remark,
                     'total_amount' => 0,
                 ]);
                 // 订单关联到当前用户
-                $order->user()->associate($user);  // 同$order->user_id = $user->id;
+                // $order->user()->associate($user);  // 同$order->user_id = $user->id;
+                $order->user_id = $user->id;
                 // 写入数据库
                 $order->save();
 
                 $totalAmount = 0;
-                $items       = $queries->items;
+                $items = $queries->items;
                 // 遍历用户提交的 商品/若为sku则改即可
                 foreach ($items as $data) {
-                    $product  = Product::find($data['product_id']);
+                    $product = Product::find($data['product_id']);
                     // 创建一个 OrderItem 并直接与当前订单关联
                     $item = $order->items()->make([
                         'amount' => $data['amount'],
-                        'price'  => $product->price,
+                        'price' => $product->price,
                     ]);
-                    $item->product()->associate($product->product_id);
+                    // $item->product()->associate($product->product_id);
+                    $item->product_id = $product->id;
                     // $item->productSku()->associate($sku);
                     $item->save();
                     $totalAmount += $product->price * $data['amount'];
+                    if ($product->decreaseStock($data['amount']) <= 0) {
+                        throw new InvalidRequestException('该商品库存不足');
+                    }
                 }
 
                 // 更新订单总金额
@@ -77,10 +87,74 @@ class OrderService extends Service
 
                 return $order;
             });
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             Log::error($e->getMessage());
             return false;
         }
         return $orderRequest;
+    }
+
+    // 改变订单状态
+    public function changeStatus($queries)
+    {
+        try {
+            switch ($queries['status']) {
+                case UnionPayCode::Success:
+                {
+                    $this->order->whereNo($queries['orderId'])->update([
+                        'status' => OrderStatusCode::StatusPending,
+                        'payment_method' => 'unionpay',
+                        'payment_no' => $queries['queryId'],
+                        'paid_at' => now()->toDateTimeString()
+                    ]);
+                }
+                case AlipayCode::TRADE_SUCCESS:
+                {
+                    $this->order->whereNo($queries['no'])->update([
+                        'status' => OrderStatusCode::StatusPending,
+                        'payment_method' => 'alipay',
+                        'payment_no' => $queries['payment_no'],
+                        'paid_at' => now()->toDateTimeString()
+                    ]);
+                }
+                case AlipayGatewayCode::PaySuccess:
+                {
+                    $this->order->whereNo($queries['order_id'])->update([
+                        'status' => OrderStatusCode::StatusPlaced,
+                        'payment_method' => 'alipay_gateway',
+                        'payment_no' => $queries['out_order_no'],
+                        'paid_at' => now()->toDateTimeString(),
+                        'extra' => json_encode([
+                            'merch_id' => $queries['merch_id'],
+                            'out_order_no' => $queries['out_order_no'],
+                            'order_id' => $queries['order_id'],
+                            'fee' => $queries['fee'],
+                            'amount' => $queries['amount'],
+                            'status' => $queries['status'],
+                            'pay_time' => $queries['pay_time']
+                        ])
+                    ]);
+                }
+                case AlipayGatewayCode::PayFaild:
+                {
+                    $this->order->whereNo($queries['order_id'])->update([
+                        'status' => OrderStatusCode::StatusReceived,
+                        'payment_method' => 'alipay_gateway',
+                        'payment_no' => $queries['out_order_no'],
+                        'paid_at' => now()->toDateTimeString(),
+                    ]);
+                }
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return false;
+        }
+        return $queries;
+    }
+
+    public function payCheck($queries)
+    {
+        $requestData = $queries->all();
+        return $this->order->whereUserId($this->user()->id)->whereNo($requestData['no'])->first();
     }
 }

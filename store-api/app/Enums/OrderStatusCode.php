401958513a1b10125d462e51afe1fe0299d10810
GabbyMrH <gabbymrh@gmail.com>
2020-08-12 20:14:14 +0800

    「Enum」 * 改善订单状态数据

--- store-api/app/Enums/OrderStatusCode.php
+++ store-api/app/Enums/OrderStatusCode.php
@@ -7,8 +7,7 @@ use BenSampo\Enum\Enum;
 
 final class OrderStatusCode extends Enum
 {
-    const StatusPlaced = 1;  // 已下单
-    const StatusPending = 2;  // 已付款
-    const StatusDeliverd = 0;  // 未付款
+    const StatusPlaced = 1;  // 已付款
+    const StatusDeliverd = 0;  // 待付款
     const StatusReceived = -1;  // 付款失败
 }

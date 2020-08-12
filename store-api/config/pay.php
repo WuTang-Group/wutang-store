0d76e1cf9e02a5913737603f0735ddd055ea041a
GabbyMrH <gabbymrh@gmail.com>
2020-08-12 20:14:41 +0800

    「Config|pay」 * 增设支付宝网关支付配置信息

--- store-api/config/pay.php
+++ store-api/config/pay.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    // 支付宝
     'alipay' => [
         'app_id' => env('ALIPAY_APP_ID'),
         'ali_public_key' => env('ALIPAY_PUBLIC_KEY'),
@@ -11,7 +12,7 @@ return [
             'file' => storage_path('logs/alipay.log'),
         ],
     ],
-
+    // 微信
     'wechat' => [
         'app_id' => '',
         'mch_id' => '',
@@ -22,4 +23,14 @@ return [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
     ],
+    // 支付网关
+    'alipay_gateway' => [
+        'merch_id' => '100101',
+        'product' => '802',
+        'key' => 'RrEUIbQ0C3JZdy9XBgZQ09ZPW22U7dNQ',
+        'bank_code' => '',
+        'notify_url' => 'http://www.baidu.com',
+        'return_url' => 'http://store.wutang.com/api/aligateway/return',
+        'extends' => '',
+    ]
 ];

db1dfdb827d41f25c08212ca1aa8408c14ee14bf
GabbyMrH <gabbymrh@gmail.com>
2020-08-13 09:59:23 +0800

    「Config」 * 跨域配置允许admin跨域

--- store-api/config/cors.php
+++ store-api/config/cors.php
@@ -21,7 +21,10 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*'],
+    'paths' => [
+        'api/*',
+        'admin/*'
+    ],
 
     /*
     * Matches the request method. `[*]` allows all methods.

b7da7f0f002dbab272b52bf5e24955b94ab48dc9
GabbyMrH <gabbymrh@gmail.com>
2020-08-13 16:24:15 +0800

    「Jobs」 * 队列测试文件

---
+++ store-api/app/Jobs/ProcessOrder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $tries = 3; // 队列最大尝试次数
+    protected $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        \Log::info($this->order);
+    }
+}

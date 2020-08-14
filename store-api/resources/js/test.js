4c8ba593f238c59001b38261af44d57affd8946e
GabbyMrH <gabbymrh@gmail.com>
2020-08-14 15:32:15 +0800

    「Web|JS」 * 示例websocket

--- store-api/resources/js/test.js
+++ store-api/resources/js/test.js
@@ -1,12 +1,13 @@
 ws = new WebSocket("ws://127.0.0.1:10086");
 // 客户端与服务端建立连接时触发
-ws.onopen = function() {
+ws.onopen = function(e) {
+    console.log(e)
     // 客户端向服务端发送数据
     ws.send('{"mode":"chats","order_id":"375"}');
 };
 // 客户端接收服务端数据时触发
 ws.onmessage = function(e) {
-    console.log(e);
+    //console.log(e);
     const data = JSON.parse(e.data);
     console.log(data)
 };

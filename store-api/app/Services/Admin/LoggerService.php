88e5e3083897641df5216b988a4cac154e67d1c2
GabbyMrH <gabbymrh@gmail.com>
2020-08-14 15:33:57 +0800

    「Service|Admin」 * 日志服务类新增相关方法

---
+++ store-api/app/Services/Admin/LoggerService.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Services\Admin;
+
+use App\Services\Service;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class LoggerService extends Service
+{
+    public const CONNECTION = 'mongodb';  // 连接名
+
+    // 日志列表
+    public function systemLogList($collection,$params)
+    {
+        $requestData = page_limit($params->all());
+        $model = DB::connection(static::CONNECTION)->collection($collection);
+        return $model->paginate($requestData['page_limit']);
+    }
+
+    // 删除日志
+    public function systemLogDestroy($collection,$params)
+    {
+        $requestData = $params->all();
+        $model = DB::connection(static::CONNECTION)->collection($collection);
+        try {
+            $res = $model->whereIn('_id',$requestData['ids'])->delete();
+        }catch (\Exception $e){
+            Log::error('日志删除失败',['message'=>$e->getMessage()]);
+            return false;
+        }
+        return $res;
+    }
+}

9c54317d32b2113354b341a05986d31d83d2a760
GabbyMrH <gabbymrh@gmail.com>
2020-08-14 15:29:35 +0800

    「helpers」 * 修复分页助手函数传参字符串的问题

--- store-api/bootstrap/helpers.php
+++ store-api/bootstrap/helpers.php
@@ -3,12 +3,16 @@
  * 自定义助手函数库
  */
 
-// 分页助手
+/**
+ * 分页助手
+ * @param null $queries
+ * @return array
+ */
 function page_limit($queries = null)
 {
     $requestData = $queries;
     // 对请求参数默认分页参数进行过滤赋值
-    $requestData['page'] = isset($queries['page']) ? $queries['page'] : 1;
-    $requestData['page_limit'] = isset($queries['page_limit']) ? $queries['page_limit'] : 20;
+    $requestData['page'] = isset($queries['page']) ? (integer)$queries['page'] : 1;
+    $requestData['page_limit'] = isset($queries['page_limit']) ? (integer)$queries['page_limit'] : 20;
     return $requestData;
 }

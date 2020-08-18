eee9232dbbc88f3761380ce52628ad8eaa5b81d4
GabbyMrH <gabbymrh@gmail.com>
2020-08-18 13:54:34 +0800

    「Request|Api」 * 购物车表单验证器

---
+++ store-api/app/Http/Requests/Api/ShopCartRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+
+use App\Http\Requests\FormRequest;
+use App\Models\Product;
+
+class ShopCartRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->route()->getActionMethod())
+        {
+            case 'store':
+            {
+                return [
+                    'product_id' => ['required',function($attribute, $value, $fail){
+                        if (!$product = Product::find($value)) {
+                            return $fail('该商品不存在');
+                        }
+                        if ($product->stock === 0) {
+                            return $fail('该商品已售完');
+                        }
+                    }],
+                    'amount' => 'required|integer'
+                ];
+            }
+            case 'delete':
+            {
+                return [
+                    'product_id' => 'required|products,id'
+                ];
+            }
+        }
+    }
+}

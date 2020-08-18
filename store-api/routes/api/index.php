56e4519cd7db9560ed83fb61958657648e0bf574
GabbyMrH <gabbymrh@gmail.com>
2020-08-18 14:39:56 +0800

    「Route|Api」 * 路由更新产品参数

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -58,6 +58,8 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         Route::get('product_category/{category_slug}', 'ProductController@getCategoryProduct')->name('product.getCategoryProduct');
         // 获取产品详情->传递slug去查询对应的产品
         Route::get('product/{product_slug}', 'ProductController@index')->name('product.index');
+        // 根据ID列表查询商品
+        Route::get('product/ids', 'ProductController@productQuery')->name('product.productQuery');
         // 获取分类故事
         Route::get('category_story/{category_slug}', 'ProductController@categoryStory')->name('product.newProduct');
         // 新品

2f858863aba3ef52f418431f4be8a9eb15e25350
GabbyMrH <gabbymrh@gmail.com>
2020-08-18 15:43:20 +0800

    「Controller|Admin」 * 角色判断中间件在控制器中的用法示例

--- store-api/app/Http/Controllers/Admin/InvitationController.php
+++ store-api/app/Http/Controllers/Admin/InvitationController.php
@@ -5,17 +5,30 @@ namespace App\Http\Controllers\Admin;
 use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CodeRequest;
-use App\Models\InvitationCode;
+
+//use App\Models\InvitationCode;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class InvitationController extends Controller
 {
-    private $invitationCode;
-    public function __construct(InvitationCode $invitationCode)
+    /*private $invitationCode;*/
+
+    public function __construct()
     {
-        $this->invitationCode = $invitationCode;
+        /*        $this->invitationCode = $invitationCode;*/
+        $this->middleware('role:admin',['except'=>'test']);
+    }
+
+    public function test()
+    {
+        dd(123);
+    }
+
+    public function test2()
+    {
+        dd('test2');
     }
 
     /**
@@ -24,10 +37,10 @@ class InvitationController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function genInvitation()
-    {
-        return response()->json(ResponseData::requestSuccess(Str::random(6)));
-    }
+    /*    public function genInvitation()
+        {
+            return response()->json(ResponseData::requestSuccess(Str::random(6)));
+        }*/
 
     /**
      *「ADMIN」Get user phone
@@ -37,22 +50,22 @@ class InvitationController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function get_user(Request $request)
-    {
-        $requestArr = page_limit($request);
-        $phone = $requestArr->phone;
-        if($phone){
-            $res = User::where('phone', 'like', '%'.$phone.'%')
-                        ->where('status', 1)
-                        ->select('id', 'phone')
-                        ->paginate($requestArr->page_limit);
-        }else{
-            $res = User::where('status', 1)
-                ->select('id', 'phone')
-                ->paginate($requestArr->page_limit);
-        }
-        return response()->json(ResponseData::requestSuccess($res));
-    }
+    /*    public function get_user(Request $request)
+        {
+            $requestArr = page_limit($request);
+            $phone = $requestArr->phone;
+            if ($phone) {
+                $res = User::where('phone', 'like', '%' . $phone . '%')
+                    ->where('status', 1)
+                    ->select('id', 'phone')
+                    ->paginate($requestArr->page_limit);
+            } else {
+                $res = User::where('status', 1)
+                    ->select('id', 'phone')
+                    ->paginate($requestArr->page_limit);
+            }
+            return response()->json(ResponseData::requestSuccess($res));
+        }*/
 
     /**
      *「ADMIN」bind code
@@ -62,13 +75,13 @@ class InvitationController extends Controller
      * @param CodeRequest $codeResquest
      * @return \Illuminate\Http\JsonResponse
      */
-    public function bindCode(CodeRequest $codeResquest)
-    {
-        $phone = $codeResquest->phone;
-        $code = $codeResquest->code;
-        $user_id = User::where('phone', $phone)->value('id');
-        InvitationCode::updateOrInsert(['user_id'=>$user_id],
-            ['code'=>$code, 'valid_until'=>'2037-12-21 12:09:02']);
-        return response()->json(ResponseData::requestSuccess());
-    }
+    /*    public function bindCode(CodeRequest $codeResquest)
+        {
+            $phone = $codeResquest->phone;
+            $code = $codeResquest->code;
+            $user_id = User::where('phone', $phone)->value('id');
+            InvitationCode::updateOrInsert(['user_id'=>$user_id],
+                ['code'=>$code, 'valid_until'=>'2037-12-21 12:09:02']);
+            return response()->json(ResponseData::requestSuccess());
+        }*/
 }

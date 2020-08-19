0581c41300d128dfeed0dce92260e7d6232f38d5
peng0228 <770601900@qq.com>
2020-08-19 19:24:38 +0800

    「Category|Admin」 * 添加分类slug输入 * 添加上级分类下拉选择 * 调整富文本显示方式为下拉调用 * 提交创建分类

--- store-admin/src/views/category/components/CreateOrUpdate.vue
+++ store-admin/src/views/category/components/CreateOrUpdate.vue
@@ -20,27 +20,64 @@
         <el-divider />
         <el-row :gutter="10">
           <el-col :span="12">
-            <el-form-item label="分类描述">
-              <tinymce v-model="form.describe" :height="50" />
+            <el-form-item label="分类slug">
+              <el-input v-model="form.slug" />
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="分类描述(英文)">
-              <tinymce v-model="form.describe_en" :height="50" />
+            <el-form-item label="上级分类">
+              <el-select v-model="form.parent_id" placeholder="请选择">
+                <el-option
+                  v-for="item in parentCategory"
+                  :key="item.id"
+                  :label="item.title"
+                  :value="item.id"
+                />
+              </el-select>
             </el-form-item>
+          </el-col>
+        </el-row>
+        <el-divider />
+
+        <el-row :gutter="10">
+          <el-col :span="12">
+            <el-form-item label="分类描述">
+              <el-input v-model="form.describe" disabled>
+                <el-button v-if="!describeVisible" slot="append" icon="el-icon-edit-outline" @click="handleDescribe" />
+                <el-button v-if="describeVisible" slot="append" icon="el-icon-arrow-up" @click="handleDescribe" />
+              </el-input>
+            </el-form-item>
+            <tinymce v-if="describeVisible" v-model="form.describe" :height="50" />
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="分类描述(英文)">
+              <el-input v-model="form.describe_en" disabled>
+                <el-button v-if="!describeEnVisible" slot="append" icon="el-icon-edit-outline" @click="handleDescribeEn" />
+                <el-button v-else slot="append" icon="el-icon-arrow-up" @click="handleDescribeEn" />
+              </el-input>
+            </el-form-item>
+            <tinymce v-if="describeEnVisible" v-model="form.describe_en" :height="50" />
           </el-col>
         </el-row>
         <el-divider />
         <el-row :gutter="10">
           <el-col :span="12">
             <el-form-item label="分类简介">
-              <tinymce v-model="form.description" :height="50" />
+              <el-input v-model="form.description" disabled>
+                <el-button v-if="!descriptionVisible" slot="append" icon="el-icon-edit-outline" @click="handleDescription" />
+                <el-button v-if="descriptionVisible" slot="append" icon="el-icon-arrow-up" @click="handleDescription" />
+              </el-input>
             </el-form-item>
+            <tinymce v-if="descriptionVisible" v-model="form.description" :height="50" />
           </el-col>
           <el-col :span="12">
             <el-form-item label="分类简介(英文)">
-              <tinymce v-model="form.description_en" :height="50" />
+              <el-input v-model="form.description_en" disabled>
+                <el-button v-if="!descriptionEnVisible" slot="append" icon="el-icon-edit-outline" @click="handleDescriptionEn" />
+                <el-button v-if="descriptionEnVisible" slot="append" icon="el-icon-arrow-up" @click="handleDescriptionEn" />
+              </el-input>
             </el-form-item>
+            <tinymce v-if="descriptionEnVisible" v-model="form.description_en" :height="50" />
           </el-col>
         </el-row>
         <el-row :gutter="10">
@@ -54,7 +91,7 @@
                 list-type="picture-card"
                 :auto-upload="false"
                 :limit="limitCount"
-                :file-list="fileList"
+                :file-list="bannerList"
                 :on-change="handleBannerChange"
                 :on-remove="handleBannerRemove"
               >
@@ -64,6 +101,25 @@
               <el-button type="primary" @click="updatetValue">测试上传</el-button>
             </el-form-item>
           </el-col>
+          <!--          <el-col :span="12">-->
+          <!--            <el-form-item label="分类简介图">-->
+          <!--              <el-upload-->
+          <!--                ref="uploadImg"-->
+          <!--                :class="{hide:hideUpload}"-->
+          <!--                name="img"-->
+          <!--                action="#"-->
+          <!--                list-type="picture-card"-->
+          <!--                :auto-upload="false"-->
+          <!--                :limit="limitCount"-->
+          <!--                :file-list="imgList"-->
+          <!--                :on-change="handleBannerChange"-->
+          <!--                :on-remove="handleBannerRemove"-->
+          <!--              >-->
+          <!--                <el-button size="small" type="primary">点击上传</el-button>-->
+          <!--                <div slot="tip" class="el-upload__tip">只能上传一张jpg/png文件，且不超过500kb</div>-->
+          <!--              </el-upload>-->
+          <!--            </el-form-item>-->
+          <!--          </el-col>-->
         </el-row>
       </el-form>
     </el-card>
@@ -72,7 +128,7 @@
 
 <script>
 import Tinymce from '@/components/Tinymce'
-import { testUpload } from '@/api/category'
+import { getList, store } from '@/api/category'
 
 export default {
   name: 'CreateOrUpdate',
@@ -82,35 +138,71 @@ export default {
   data() {
     return {
       form: {
+        title: '',
+        title_en: '',
+        slug: '',
         describe: '',
         describe_en: '',
         description: '',
-        description_en: ''
+        description_en: '',
+        banner: '',
+        parent_id: '',
+        img: ''
       },
+      parentCategory: null,
       hideUpload: false,
       limitCount: 1,
-      fileList: []
+      bannerList: [],
+      imgList: [],
+      describeVisible: false,
+      describeEnVisible: false,
+      descriptionVisible: false,
+      descriptionEnVisible: false
     }
   },
   watch: {
   },
   created() {
+    this.getCategoryList()
   },
   methods: {
     updatetValue() {
-      testUpload({ data: this.fileList }).then(response => {
+      console.log(this.form)
+      store(this.form).then(response => {
         console.log(response)
+      }
+      )
+    },
+    getCategoryList() {
+      const param = {
+        page_limit: 40
+      }
+      getList(param).then(response => {
+        console.log(response.data)
+        this.parentCategory = response.data.data
       })
     },
     handleBannerChange(file, fileList) {
       this.hideUpload = fileList.length >= this.limitCount
       // const form = new FormData()
       // form.append('name', file)
-      this.fileList = fileList
-      console.log(file)
+      this.bannerList = fileList
+      this.form.banner = file
     },
     handleBannerRemove(file, fileList) {
       this.hideUpload = fileList.length >= this.limitCount
+    },
+    handleDescribe() {
+      this.describeVisible = !this.describeVisible
+    },
+    handleDescribeEn() {
+      this.describeEnVisible = !this.describeEnVisible
+    },
+    handleDescription() {
+      this.descriptionVisible = !this.descriptionVisible
+    },
+    handleDescriptionEn() {
+      this.descriptionEnVisible = !this.descriptionEnVisible
     }
   }
 }

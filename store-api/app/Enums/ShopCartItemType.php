0a84223ec319cd5a4ea61e7e62b59d06cbb149a9
GabbyMrH <gabbymrh@gmail.com>
2020-08-19 11:06:25 +0800

    「Enum」 * 购物车项目type参数说明

--- store-api/app/Enums/ShopCartItemType.php
+++ store-api/app/Enums/ShopCartItemType.php
@@ -7,7 +7,7 @@ use BenSampo\Enum\Enum;
 
 final class ShopCartItemType extends Enum
 {
-    const AddItem = 'add';
-    const ReduceItem = 'reduce';
-    const FixedItem = 'fixed';
+    const AddItem = 'add';  // 点击+号递增时
+    const ReduceItem = 'reduce';  // 点击-号递减时
+    const FixedItem = 'fixed';  // 传递固定数量时
 }

3ea5f1d7f164170f478709006cc9b728757bc339
GabbyMrH <gabbymrh@gmail.com>
2020-08-20 11:55:03 +0800

    「Controller|Api」 * 用户心愿单控制器及相关方法

---
+++ store-api/app/Http/Controllers/Api/WishListController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Handlers\ResponseData;
+use App\Http\Controllers\Controller;
+use App\Services\Api\WishListService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * @group [API] User wish lish
+ * 用户心愿单
+ * @authenticated
+ * @package App\Http\Controllers\Api
+ */
+class WishListController extends Controller
+{
+    private $service;
+
+    public function __construct(WishListService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Get wish list
+     * 获取心愿单商品列表
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $results = $this->service->index($request);
+        return response()->json(ResponseData::requestSuccess($results));
+    }
+
+    /**
+     * Request Add product to wish list
+     * 请求将商品加入心愿单
+     * @queryParam product_id integer required 商品id
+     * @param $product_id
+     * @param Request $request
+     * @return Application|ResponseFactory|Response
+     */
+    public function store($product_id,Request $request)
+    {
+        $results = $this->service->store($product_id,$request);
+        return response(ResponseData::requestSuccess($results));
+    }
+
+    /**
+     * Remove product from wish list
+     * 移除心愿单商品
+     * @queryParam product_id required 商品id
+     * @param $product_id
+     * @param Request $request
+     * @return Application|ResponseFactory|Response
+     */
+    public function delete($product_id,Request $request)
+    {
+        $results = $this->service->delete($product_id,$request);
+        return response(ResponseData::requestSuccess($results));
+    }
+}

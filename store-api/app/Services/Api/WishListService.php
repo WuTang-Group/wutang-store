1c17ab7b65aa187e2980d0976170d0a6c553c266
GabbyMrH <gabbymrh@gmail.com>
2020-08-20 11:55:19 +0800

    「Service|Api」 * 用户心愿单服务类及相关方法

---
+++ store-api/app/Services/Api/WishListService.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Services\Api;
+
+use App\Services\Service;
+
+class WishListService extends Service
+{
+    // 心愿单列表
+    public function index($params)
+    {
+        return $params->user()->wishLists()->get();
+    }
+
+    // 加入心愿单
+    public function store($productId,$params)
+    {
+        $user = $params->user();
+        //attach将用户与商品关联
+        // 判断用户若与商品并没关联则进行关联
+        if ($user->wishLists()->find($productId)) {
+            return [];
+        }
+
+        $user->wishLists()->attach($productId);
+
+        return [];
+    }
+
+    // 移出心愿单
+    public function delete($productId,$params)
+    {
+        $user = $params->user();
+        $user->favoriteProducts()->detach($productId);  // detach取消多对多关联
+        return [];
+    }
+}

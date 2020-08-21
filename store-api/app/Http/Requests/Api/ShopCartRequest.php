4b46f3d8ddcfe9b37db4e9908cbd3341a50419c6
GabbyMrH <gabbymrh@gmail.com>
2020-08-21 13:04:29 +0800

    「Request|APi」 * 购物车验证器修复传值问题

--- store-api/app/Http/Requests/Api/ShopCartRequest.php
+++ store-api/app/Http/Requests/Api/ShopCartRequest.php
@@ -19,6 +19,30 @@ class ShopCartRequest extends FormRequest
     public function rules()
     {
         switch ($this->route()->getActionMethod()) {
+            case 'store':
+            {
+                return [
+                    'product_list' => 'required|array',
+                    'product_list.*.product_id' => ['required', function ($attribute, $value, $fail) {
+                        if (!$product = Product::find($value)) {
+                            return $fail('该商品不存在'.$value);
+                        }
+                        if ($product->stock === 0) {
+                            return $fail('该商品已售完'.$value);
+                        }
+                        if (ShopCartItem::whereUserId($this->user('api')->id)->whereProductId($value)->first()) {
+                            return $fail('您购物车已有该商品'.$value);
+                        }
+                        // 判断用户是否买过该商品
+                        $orderItem = Order::whereHas('items', function ($query) use ($value) {
+                            $query->whereProductId($value);
+                        })->with('items')->whereUserId($this->user()->id)->get();
+                        if (!$orderItem->isEmpty()) {
+                            return $fail('您已购买过该商品'.$value);
+                        }
+                    }]
+                ];
+            }
 //            case 'store':
 //            {
 //                return [
@@ -42,30 +66,6 @@ class ShopCartRequest extends FormRequest
 //                    }]
 //                ];
 //            }
-            case 'store':
-            {
-                return [
-                    'product_list' => 'required' | 'array',
-                    'product_list.*.product_id' => ['required', function ($attribute, $value, $fail) {
-                        if (!$product = Product::find($value)) {
-                            return $fail('该商品不存在');
-                        }
-                        if ($product->stock === 0) {
-                            return $fail('该商品已售完');
-                        }
-                        if (ShopCartItem::whereUserId($this->user('api')->id)->whereProductId($value)->first()) {
-                            return $fail('您购物车已有该商品');
-                        }
-                        // 判断用户是否买过该商品
-                        $orderItem = Order::whereHas('items', function ($query) use ($value) {
-                            $query->whereProductId($value);
-                        })->with('items')->whereUserId($this->user()->id)->get();
-                        if (!$orderItem->isEmpty()) {
-                            return $fail('您已购买过该商品');
-                        }
-                    }]
-                ];
-            }
             case 'updateItemNumber':
             {
                 return [

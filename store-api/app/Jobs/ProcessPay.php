d231f961c895a109ef8b9f23ba154734064a838c
GabbyMrH <gabbymrh@gmail.com>
2020-08-21 17:44:10 +0800

    「Job|ProcessPay」 * 支付队列文件

---
+++ store-api/app/Jobs/ProcessPay.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Payments\AlipayGateway;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessPay implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $tries = 3; // 队列最大尝试次数
+    public $timeout = 10; // 超时
+
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+    }
+}

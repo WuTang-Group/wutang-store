0fbd27096ea639c33b16e8b5293161cd8c74925f
GabbyMrH <gabbymrh@gmail.com>
2020-08-21 13:02:02 +0800

    「Request|APi」 * 订单验证器移除items验证

--- store-api/app/Http/Requests/Api/OrderRequest.php
+++ store-api/app/Http/Requests/Api/OrderRequest.php
@@ -24,27 +24,27 @@ class OrderRequest extends FormRequest
                 return [
                     // 判断用户提交的地址 ID 是否存在于数据库并且属于当前用户
                     'address_id' => ['required',Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id),],
-                    'items' => 'required|array',
-                    'items.*.product_id' => [ // 检查 items 数组下每一个子数组的 product_id 参数
-                        'required',
-                        function ($attribute, $value, $fail) {
-                            if (!$product = Product::find($value)) {
-                                return $fail('该商品不存在');
-                            }
-                            if ($product->stock === 0) {
-                                return $fail('该商品已售完');
-                            }
-                            // 获取当前索引
-                            preg_match('/items\.(\d+)\.product_id/', $attribute, $m);
-                            $index = $m[1];
-                            // 根据索引找到用户所提交的购买数量
-                            $amount = $this->input('items')[$index]['amount'];
-                            if ($amount > 0 && $amount > $product->stock) {
-                                return $fail('该商品库存不足');
-                            }
-                        },
-                    ],
-                    'items.*.amount' => ['required', 'integer', 'min:1'],
+//                    'items' => 'required|array',
+//                    'items.*.product_id' => [ // 检查 items 数组下每一个子数组的 product_id 参数
+//                        'required',
+//                        function ($attribute, $value, $fail) {
+//                            if (!$product = Product::find($value)) {
+//                                return $fail('该商品不存在');
+//                            }
+//                            if ($product->stock === 0) {
+//                                return $fail('该商品已售完');
+//                            }
+//                            // 获取当前索引
+//                            preg_match('/items\.(\d+)\.product_id/', $attribute, $m);
+//                            $index = $m[1];
+//                            // 根据索引找到用户所提交的购买数量
+//                            $amount = $this->input('items')[$index]['amount'];
+//                            if ($amount > 0 && $amount > $product->stock) {
+//                                return $fail('该商品库存不足');
+//                            }
+//                        },
+//                    ],
+//                    'items.*.amount' => ['required', 'integer', 'min:1'],
                 ];
             }
             case 'payCheck':

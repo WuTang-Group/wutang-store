1d01f408307c447ea17a8e3b95c2c0e53e80e294
JML Tech <johnlin.jmltech@gmail.com>
2020-08-21 18:48:05 +0800

    添加与删除多语言文件

---
+++ store-api/public/assets/js/lang/cn.js
@@ -0,0 +1,9 @@
+lang = {
+    logged_in : '登录成功。',
+	logged_out : '登出成功。',
+    reset : '您的账号密码已更改成功!',
+    item_added : '商品已加入您的购物车。',
+    item_exist : '您购物车已有该商品。',
+    item_removed : '商品已从您的购物车删除。',
+    please_select : '请选择',
+};

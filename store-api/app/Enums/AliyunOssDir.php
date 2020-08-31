94153b76032329cb26fdb8604678d43576efa828
peng0228 <770601900@qq.com>
2020-08-31 18:03:53 +0800

    「Enums|Api」 * 添加图片资源OSS文件夹枚举类型

--- store-api/app/Enums/AliyunOssDir.php
+++ store-api/app/Enums/AliyunOssDir.php
@@ -10,4 +10,5 @@ final class AliyunOssDir extends Enum
     const Product = 'Products';
     const ProductCategory = 'ProductCategories';
     const ProductCategoryStory = 'ProductCategoryStories';
+    const AssetImg = 'AssetImgs';
 }

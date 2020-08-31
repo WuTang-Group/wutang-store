9e5b9a22eeff5d075b274a3e25042a3b15129ee5
GabbyMrH <gabbymrh@gmail.com>
2020-08-31 17:54:22 +0800

    「API|Databse-factory」 * ProductCategoriesFactory:更改模拟种子数据文件

--- store-api/database/factories/ProductCategoriesFactory.php
+++ store-api/database/factories/ProductCategoriesFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\ProductCategory::class, function (Faker $faker) {
     return [
-        'title'=>$faker->randomElement(['面膜', '洁面', '防晒', '爽肤水', '眼霜', '乳液', '面霜', '精华', '卸妆', '男士护肤', '眼线', '粉底液', 'BB霜', '隔离', '睫毛膏', '彩妆盘', '唇膏', '腮红', '香水', '精油', '身体护理']),
+        'title'=>$faker->randomElement(['面膜', '洁面', '防晒', '爽肤水', '眼霜', '乳液', '面霜', '精华', '卸妆', '男士护肤', '眼线', '粉底液', 'BB霜', '隔离', '睫毛膏', '彩妆盘', '唇膏', '腮红', '香水', '精油', '身体护理','套盒']),
         'slug' => \Str::slug($faker->words(2, true)),
         'title_en'=>$faker->words(3, true),
         'describe'=>$faker->text ,
@@ -16,7 +16,7 @@ $factory->define(\App\Models\ProductCategory::class, function (Faker $faker) {
         'description'=>$faker->sentence(5, true),
         'description_en'=>$faker->sentence(5, true),
         'img'=>$faker->imageUrl(),
-        'parent_id'=>$faker->numberBetween(1, 20),
+        'parent_id'=>$faker->numberBetween(0, 30),
         'created_at' => $faker->dateTimeThisMonth(),
         'updated_at' => $faker->dateTimeThisMonth()
     ];

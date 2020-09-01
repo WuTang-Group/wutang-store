857f2a14ace2630cbd0b1aa21035e27fb29bcd37
GabbyMrH <gabbymrh@gmail.com>
2020-09-01 16:58:44 +0800

    「API|Models」 * 新增支付配置模型

---
+++ store-api/app/Models/AlipayBankGateway.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class AlipayBankGateway extends Model
+{
+    protected $fillable = [
+        ''
+    ];
+}
---
+++ store-api/app/Models/AlipayLegacyExpress.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class AlipayLegacyExpress extends Model
+{
+    //
+}

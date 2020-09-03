70ac22730b1cf57a13223ef2f0ec06dd41e1d984
GabbyMrH <gabbymrh@gmail.com>
2020-09-03 15:37:54 +0800

    「API|Cache-Contracts」 * 新增基础缓存接口

---
+++ store-api/app/Caches/Contracts/BaseCacheInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Caches\Contracts;
+
+
+interface BaseCacheInterface
+{
+    /**
+     * 写入缓存
+     * @param $params
+     * @return mixed
+     */
+    public function create($params);
+
+    /**
+     * 更新缓存
+     * @param $params
+     * @return mixed
+     */
+    public function update($params);
+
+    /**
+     * 删除缓存
+     * @param $params
+     * @return mixed
+     */
+    public function delete($params);
+}

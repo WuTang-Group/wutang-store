cc42948db3603e00d29695e588ed3582401d2569
GabbyMrH <gabbymrh@gmail.com>
2020-09-03 15:32:10 +0800

    「API|Models」 * 支付宝即时到账模型增加加密显示数据

--- store-api/app/Models/AlipayLegacyExpress.php
+++ store-api/app/Models/AlipayLegacyExpress.php
@@ -4,5 +4,12 @@ namespace App\Models;
 
 class AlipayLegacyExpress extends Model
 {
-    //
+    protected $fillable = [
+        'pid','key','seller_email','return_url','notify_url','status'
+    ];
+    // 对查询对key值进行加密
+    public function getKeyAttribute()
+    {
+        return $this->attributes['key'] = encrypt($this->attributes['key']);
+    }
 }

2773e2a26edbd49ca83da82becd7759d21ca724d
GabbyMrH <gabbymrh@gmail.com>
2020-09-03 16:43:19 +0800

    「API|Route|admin」 * 新增状态更新路由

--- store-api/routes/admin/payment.php
+++ store-api/routes/admin/payment.php
@@ -5,5 +5,6 @@
  */
 Route::get('alipay/legacy_express','Payment\AlipayLegacyExpressController@index')->name('alipay.legacy_express.index');
 Route::post('alipay/legacy_express','Payment\AlipayLegacyExpressController@store')->name('alipay.legacy_express.store');
-Route::put('alipay/legacy_express','Payment\AlipayLegacyExpressController@update')->name('alipay.legacy_express.update');
-Route::delete('alipay/legacy_express','Payment\AlipayLegacyExpressController@delete')->name('alipay.legacy_express.delete');
+Route::put('alipay/legacy_express/{id}','Payment\AlipayLegacyExpressController@update')->name('alipay.legacy_express.update');
+Route::delete('alipay/legacy_express/{id}','Payment\AlipayLegacyExpressController@delete')->name('alipay.legacy_express.delete');
+Route::put('alipay/legacy_express/status/{id}','Payment\AlipayLegacyExpressController@updateStatus')->name('alipay.legacy_express.status.updateStatus');

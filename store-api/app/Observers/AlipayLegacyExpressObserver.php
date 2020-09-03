425f820cad971f333029b03d14a3d869d5c4a093
GabbyMrH <gabbymrh@gmail.com>
2020-09-03 15:35:45 +0800

    「API|Observer」 * 新增AlipayLegacyExpress模型观察者以及自动化缓存

---
+++ store-api/app/Observers/AlipayLegacyExpressObserver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Observers;
+
+use App\Caches\AlipayLegacyExressCache;
+use App\Models\AlipayLegacyExpress;
+
+class AlipayLegacyExpressObserver
+{
+    private $cache;
+
+    public function __construct(AlipayLegacyExressCache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * Handle the alipay legacy express "created" event.
+     *
+     * @param AlipayLegacyExpress $alipayLegacyExpress
+     * @return void
+     */
+    public function created(AlipayLegacyExpress $alipayLegacyExpress)
+    {
+        $this->cache->create($alipayLegacyExpress);
+    }
+
+    /**
+     * Handle the alipay legacy express "updated" event.
+     *
+     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @return void
+     */
+    public function updated(AlipayLegacyExpress $alipayLegacyExpress)
+    {
+        $this->cache->update($alipayLegacyExpress);
+    }
+
+    /**
+     * Handle the alipay legacy express "deleted" event.
+     *
+     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @return void
+     */
+    public function deleted(AlipayLegacyExpress $alipayLegacyExpress)
+    {
+        $this->cache->delete($alipayLegacyExpress);
+    }
+
+    /**
+     * Handle the alipay legacy express "restored" event.
+     *
+     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @return void
+     */
+    public function restored(AlipayLegacyExpress $alipayLegacyExpress)
+    {
+        //
+    }
+
+    /**
+     * Handle the alipay legacy express "force deleted" event.
+     *
+     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @return void
+     */
+    public function forceDeleted(AlipayLegacyExpress $alipayLegacyExpress)
+    {
+        //
+    }
+}

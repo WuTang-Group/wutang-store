7113e726d8c4fb9bdb2e9225713f0036dda9d04c
GabbyMrH <gabbymrh@gmail.com>
2020-09-07 15:16:38 +0800

    「API|Database|migration」 * 商品分类迁移文件字段变化(注意查看具体内容)

--- store-api/database/migrations/2020_07_24_110626_create_product_categories_table.php
+++ store-api/database/migrations/2020_07_24_110626_create_product_categories_table.php
@@ -15,15 +15,16 @@ class CreateProductCategoriesTable extends Migration
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->comment('分类名称');
             $table->string('slug')->index()->nullable()->comment('分类slug');
-            $table->string('title')->nullable()->comment('分类名称');
-            $table->string('title_en')->nullable()->comment('英文分类名称');
+            $table->string('thumbnail')->nullable()->comment('分类缩略图(显示于系列概述处)');
+            $table->string('banner')->nullable()->comment('分类banner图(显示于分类介绍顶部)');
+            $table->string('title')->nullable()->comment('分类介绍标题');
+            $table->string('title_en')->nullable()->comment('分类英文标题');
+            $table->string('sub_title')->nullable()->comment('分类副标题');
             $table->text('describe')->nullable()->comment('分类描述');
-            $table->text('describe_en')->nullable()->comment('英文分类描述');
-            $table->string('banner')->nullable()->comment('分类banner图');
-            $table->text('description')->nullable()->comment('分类简介');
-            $table->text('description_en')->nullable()->comment('英文分类简介');
-            $table->string('img')->nullable()->comment('分类简介图');
+            $table->text('describe_en')->nullable()->comment('分类英文描述');
+            $table->text('describe_img')->nullable()->comment('分类描述图(显示于中下描述位置,与描述匹配)');
             $table->unsignedBigInteger('parent_id')->default(0)->comment('分类从属关系,0为默认一级分类');
             $table->timestamps();
         });

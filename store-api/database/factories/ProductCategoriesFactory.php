0f46e2c69361e5128c25c867c58e24eaa1cabf6c
GabbyMrH <gabbymrh@gmail.com>
2020-09-07 15:17:08 +0800

    「API|Database|factory」 * 商品分类工厂模型数据变更

--- store-api/database/factories/ProductCategoriesFactory.php
+++ store-api/database/factories/ProductCategoriesFactory.php
@@ -7,15 +7,16 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\ProductCategory::class, function (Faker $faker) {
     return [
-        'title'=>$faker->randomElement(['套装','面膜', '洁面', '套装B', '爽肤水', '眼霜', '套装A', '面霜', '套装C','眼线', '粉底液', 'BB霜', '隔离', '睫毛膏', '彩妆盘', '唇膏', '腮红', '香水', '精油', '身体护理']),
+        'name'=>$faker->randomElement(['套装系列','护肤系列','面膜', '洁面', '套装B', '爽肤水', '眼霜', '套装A', '面霜', '套装C','眼线', '粉底液', 'BB霜', '隔离', '睫毛膏', '彩妆盘', '唇膏', '腮红', '香水', '精油', '身体护理']),
         'slug' => \Str::slug($faker->words(2, true)),
+        'thumbnail' => $faker->imageUrl(),
+        'banner'=>$faker->imageUrl(),
+        'title' => $faker->words(3, true),
         'title_en'=>$faker->words(3, true),
+        'sub_title' => $faker->words(3, true),
         'describe'=>$faker->text ,
         'describe_en'=>$faker->text ,
-        'banner'=>$faker->imageUrl(),
-        'description'=>$faker->sentence(5, true),
-        'description_en'=>$faker->sentence(5, true),
-        'img'=>$faker->imageUrl(),
+        'describe_img' => $faker->imageUrl(),
         'parent_id'=>$faker->numberBetween(0, 30),
         'created_at' => $faker->dateTimeThisMonth(),
         'updated_at' => $faker->dateTimeThisMonth()

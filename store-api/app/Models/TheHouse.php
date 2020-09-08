4072bd8f9e6d6d78aa9951f7a8f56638b8fd0eb1
GabbyMrH <gabbymrh@gmail.com>
2020-09-08 15:19:54 +0800

    TheHouse * TheHouse 模型

---
+++ store-api/app/Models/TheHouse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+class TheHouse extends Model
+{
+    protected $fillable = [
+        'the_house_category_id', 'banner', 'title', 'sub_title', 'describe'
+    ];
+}

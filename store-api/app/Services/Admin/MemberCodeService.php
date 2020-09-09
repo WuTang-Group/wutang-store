c51a47dd884da50b11d8ed751f9a2318dbd32800
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 15:58:28 +0800

    Services * 会员码服务类:新增列表方法

---
+++ store-api/app/Services/Admin/MemberCodeService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Services\Admin;
+
+use App\Models\MemberCode;
+use App\Services\Service;
+
+class MemberCodeService extends Service
+{
+    private $memberCode;
+
+    public function __construct(MemberCode $memberCode)
+    {
+        $this->memberCode = $memberCode;
+    }
+
+    // 会员码无限级列表-默认从0(一级)开始获取
+    public function index($params)
+    {
+        $requestData = $params->only('parent_id');
+        $parentId = $requestData['parent_id'] ?? 0;
+        return MemberCode::with(['children'])->whereParentId($parentId)->get();
+    }
+}

e7f68ecd8fb1d02163d5e4b54900bf323f2f6ca8
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 17:24:07 +0800

    Models * 开启数据写入

--- store-api/app/Models/Company.php
+++ store-api/app/Models/Company.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 class Company extends Model
 {
+    protected $fillable = [
+        'name', 'code', 'type', 'address', 'tel_number', 'contact_person', 'parent_id',
+        'level', 'status', 'remark'
+    ];
+
     /**
      * 一对多关联部门模型
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
--- store-api/app/Models/Department.php
+++ store-api/app/Models/Department.php
@@ -4,5 +4,7 @@ namespace App\Models;
 
 class Department extends Model
 {
-    //
+    protected $fillable = [
+        'company_id', 'name', 'code', 'leader', 'parent_id', 'level', 'status', 'remark'
+    ];
 }

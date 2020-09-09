0323dd5c4dfddebf07e8d70f6c16d1c71cf4363d
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 17:48:51 +0800

    Models * 定义无限级关联关系

--- store-api/app/Models/Company.php
+++ store-api/app/Models/Company.php
@@ -17,4 +17,29 @@ class Company extends Model
     {
         return $this->hasMany(Department::class);
     }
+
+    /**
+     * 从属关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent() {
+        return $this->belongsTo(Department::class, 'parent_id');
+    }
+
+    /**
+     * 一对多关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function child() {
+        return $this->hasMany(Department::class, 'parent_id');
+    }
+
+    /**
+     * 关联child无限级数据
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children()
+    {
+        return $this->child()->with(['children']);
+    }
 }
--- store-api/app/Models/Department.php
+++ store-api/app/Models/Department.php
@@ -7,4 +7,29 @@ class Department extends Model
     protected $fillable = [
         'company_id', 'name', 'code', 'leader', 'parent_id', 'level', 'status', 'remark'
     ];
+
+    /**
+     * 从属关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent() {
+        return $this->belongsTo(Department::class, 'parent_id');
+    }
+
+    /**
+     * 一对多关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function child() {
+        return $this->hasMany(Department::class, 'parent_id');
+    }
+
+    /**
+     * 关联child无限级数据
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children()
+    {
+        return $this->child()->with(['children']);
+    }
 }

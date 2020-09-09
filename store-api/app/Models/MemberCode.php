c6c1aba826cb40aad602e450199ecfe34897d60f
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 15:52:36 +0800

    Models * MemberCode模型增加无限级分类查询定义

--- store-api/app/Models/MemberCode.php
+++ store-api/app/Models/MemberCode.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 class MemberCode extends Model
 {
+    protected $fillable = [
+        'code', 'user_name', 'parent_id', 'status', 'remark','level'
+    ];
+
     /**
      * 一对多关联用户模型(一个会员码下有多个用户)
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -11,5 +15,30 @@ class MemberCode extends Model
     public function users()
     {
         return $this->hasMany(User::class);
+    }
+
+    /**
+     * 从属关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent() {
+        return $this->belongsTo(MemberCode::class, 'parent_id');
+    }
+
+    /**
+     * 一对多关联本身parent id
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function child() {
+        return $this->hasMany(MemberCode::class, 'parent_id');
+    }
+
+    /**
+     * 关联child无限级数据
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children()
+    {
+        return $this->child()->with(['children']);
     }
 }

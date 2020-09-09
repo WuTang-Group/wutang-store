b7b83533b57953d6b0dd530e1c1a6382268e90c4
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 11:54:26 +0800

    migrations * 会员码表更改user_id为user_name * 用以明确会员码所属用户的真实姓名

--- store-api/database/migrations/2020_07_21_102544_create_member_codes_table.php
+++ store-api/database/migrations/2020_07_21_102544_create_member_codes_table.php
@@ -16,7 +16,7 @@ class CreateMemberCodesTable extends Migration
         Schema::create('member_codes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('code')->comment('会员码');
-            $table->unsignedBigInteger('user_id')->nullable()->comment('所属users表id(可通过用户查询用户所对应的会员码)');
+            $table->string('user_name')->nullable()->comment('会员码所属用户的姓名');
             $table->integer('parent_id')->default(0)->comment('会员码上级,0为最高级');
             $table->integer('status')->default(1)->comment('状态:1启用,-1未启用');
             $table->text('remark')->nullable()->comment('其他说明');

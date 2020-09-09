88251fc089a49ec82804ce124c56e7c0a00a8649
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 11:35:08 +0800

    Models * Company:定义一对多关联部门模型

--- store-api/app/Models/Company.php
+++ store-api/app/Models/Company.php
@@ -4,5 +4,12 @@ namespace App\Models;
 
 class Company extends Model
 {
-    //
+    /**
+     * 一对多关联部门模型
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function departments()
+    {
+        return $this->hasMany(Department::class);
+    }
 }

d309c31aa913af531ee91f4f0be01b2bf4e00f5c
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 15:49:19 +0800

    seeds * 填入MemberCode种子

--- store-api/database/seeds/DatabaseSeeder.php
+++ store-api/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(MemberCodesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(UserAddressTableSeeder::class);
         $this->call(UserProfileTableSeeder::class);

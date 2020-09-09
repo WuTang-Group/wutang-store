56291b7cea0f916d08b47adcaabfa7ed59319726
GabbyMrH <gabbymrh@gmail.com>
2020-09-09 11:35:40 +0800

    Models * MemberCode:定义一对多关联用户模型

--- store-api/app/Models/MemberCode.php
+++ store-api/app/Models/MemberCode.php
@@ -4,5 +4,12 @@ namespace App\Models;
 
 class MemberCode extends Model
 {
-    //
+    /**
+     * 一对多关联用户模型(一个会员码下有多个用户)
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }

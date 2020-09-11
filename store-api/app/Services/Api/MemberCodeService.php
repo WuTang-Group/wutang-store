7b3c3382acb432e9df579f475e4eb9018a883f39
GabbyMrH <gabbymrh@gmail.com>
2020-09-11 18:08:49 +0800

    Services * 会员码服务类

---
+++ store-api/app/Services/Api/MemberCodeService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Handlers\MemberCodeHandler;
+use App\Models\MemberCode;
+use App\Services\Service;
+use Illuminate\Support\Facades\Log;
+
+class MemberCodeService extends Service
+{
+    private $memberCode;
+
+    public function __construct(MemberCode $memberCode)
+    {
+        $this->memberCode = $memberCode;
+    }
+
+    // 为用户生成会员码(前端api)-用户提交资料后调用此接口
+    public function generateMemberCode()
+    {
+        try {
+            $member_code_query = $this->memberCode->firstWhere('user_id', $this->user()->id);
+            if (!is_null($member_code_query)) {
+                return false;
+            }
+            return $this->memberCode->create([
+                'code' => MemberCodeHandler::generateCode(),
+                'user_id' => $this->user()->id,
+            ]);
+        } catch (\Exception $e) {
+            Log::error('会员码生成失败', ['message' => $e->getMessage()]);
+            return false;
+        }
+    }
+
+    // 绑定上级会员码
+    public function bindParentCode($params)
+    {
+        try {
+            // 上级
+            $parent_code = $this->memberCode->whereCode($params->code)->firstOrFail();
+            // 本级
+            $current_code = $this->memberCode->where('user_id', $this->user()->id)->firstOrFail();
+
+            $current_code->parent_id = $parent_code->parent_id;  // 关联上级code
+            $current_code->level = $parent_code->level + 1;  // 等级在上级基础+1
+            $current_code->save();
+        } catch (\Exception $e) {
+            Log::error('会员码绑定失败', ['message' => $e->getMessage()]);
+            return false;
+        }
+    }
+}

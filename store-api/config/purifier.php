f7ff6825a32ed2967f46651c5c9e4b14e34a2af0
GabbyMrH <gabbymrh@gmail.com>
2020-09-11 14:42:58 +0800

    XSS配置修改

--- store-api/config/purifier.php
+++ store-api/config/purifier.php
@@ -100,7 +100,7 @@ return [
 //        'custom_elements' => [
 //            ['u', 'Inline', 'Inline', 'Common'],
 //        ],
-        'product_short_description' => [
+        'short_description' => [
             'HTML.Doctype'             => 'XHTML 1.0 Transitional',
             'HTML.Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,ol[start],li,p[style],br,span[style],img[width|height|alt|src],*[style|class],pre,hr,code,h2,h3,h4,h5,h6,blockquote,del,table,thead,tbody,tr,th,td',
             'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,margin,width,height,font-family,text-decoration,padding-left,color,background-color,text-align',

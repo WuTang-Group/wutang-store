f5178efa12356ae1d01b377cc03bd897b300708e
GabbyMrH <gabbymrh@gmail.com>
2020-09-11 14:38:56 +0800

    the_house表 * 新增slug标记

--- store-api/database/migrations/2020_09_08_141648_create_the_houses_table.php
+++ store-api/database/migrations/2020_09_08_141648_create_the_houses_table.php
@@ -15,12 +15,13 @@ class CreateTheHousesTable extends Migration
     {
         Schema::create('the_houses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('slug')->nullable()->comment('the house slug');
             $table->unsignedBigInteger('the_house_category_id')->nullable()->comment('从属关联the_house_category表id');
             $table->foreign('the_house_category_id')->references('id')->on('the_house_categories');
             $table->string('banner')->nullable()->comment('the house banner图');
             $table->text('title')->nullable()->comment('文案大标题');
             $table->text('sub_title')->nullable()->comment('文案副标题');
-            $table->text('describe')->nullable()->comment('简要描述，首字变大');
+            $table->text('describe')->comment('简要描述，首字变大');
             $table->timestamps();
         });
     }

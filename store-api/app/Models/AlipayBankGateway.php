a75fc13668e475c0c2a0bb7cb30a11836bbd2453
GabbyMrH <gabbymrh@gmail.com>
2020-09-14 16:52:08 +0800

    Models * 定义从属关联Payment模型

--- store-api/app/Models/AlipayBankGateway.php
+++ store-api/app/Models/AlipayBankGateway.php
@@ -7,4 +7,13 @@ class AlipayBankGateway extends Model
     protected $fillable = [
         ''
     ];
+
+    /**
+     * 从属关联Payment模型
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
 }

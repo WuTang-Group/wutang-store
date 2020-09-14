924d752d9c233880b6b36ade90b72cbdfe0997b0
GabbyMrH <gabbymrh@gmail.com>
2020-09-14 16:03:56 +0800

    migrations * 新增payments表

---
+++ store-api/database/migrations/2020_09_01_135250_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type')->comment('支付类型');
+            $table->string('img')->comment('前端显示图片');
+            $table->integer('status')->default(1)->comment('启用状态,1激活,-1未激活');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}

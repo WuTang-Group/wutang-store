0fc35f98c849bfb5a34a6470aae05a3dddcac207
GabbyMrH <gabbymrh@gmail.com>
2020-09-14 09:13:53 +0800

    AuthRequest * 修改参数提示

--- store-api/app/Http/Requests/Api/AuthRequest.php
+++ store-api/app/Http/Requests/Api/AuthRequest.php
@@ -16,52 +16,51 @@ class AuthRequest extends FormRequest
      */
     public function rules()
     {
-        switch ($this->route()->getActionMethod())
-        {
+        switch ($this->route()->getActionMethod()) {
             case 'login':
             {
                 return [
-                    'username'=>'required|string',
-                    'password'=>'required|alpha_dash|min:6',
+                    'username' => 'required|string',
+                    'password' => 'required|alpha_dash|min:6',
                 ];
             }
             case 'register':
             {
                 return [
                     'name' => 'required',
-                    'username'=>['required','min:6','regex:/^(([0-9])|([a-z])|([a-z]+[0-9]+)|([0-9]+[a-z]+))[a-z0-9]*$/i'],
-                    'password'=>'required|alpha_dash|min:6|confirmed',
+                    'username' => ['required', 'min:6', 'regex:/^(([0-9])|([a-z])|([a-z]+[0-9]+)|([0-9]+[a-z]+))[a-z0-9]*$/i'],
+                    'password' => 'required|alpha_dash|min:6|confirmed',
                     'password_confirmation' => 'required|same:password',
 //                    'invitation_code' => 'required|string',
                     'captcha_key' => 'required|string',
                     'captcha_code' => 'required|string',
-                    'password_question_id'=>'required|exists:password_questions,id',
-                    'password_answer'=>'required|string'
+                    'password_question_id' => 'required|exists:password_questions,id',
+                    'password_answer' => 'required|string'
                 ];
             }
             case 'getUserQuestion':
             {
                 return [
-                  'username' => 'required|exists:users,username'
+                    'username' => 'required|exists:users,username'
                 ];
             }
 
             case 'resetPassword':
             {
                 return [
-                    'username'=>'required|exists:users,username',
-                    'password_question_id'=>'required',
-                    'password_answer'=>'required|string',
-                    'password'=>'required|alpha_dash|min:6|confirmed',
-                    'password_confirmation'=>'required|same:password',
+                    'username' => 'required|exists:users,username',
+                    'password_question_id' => 'required',
+                    'password_answer' => 'required|string',
+                    'password' => 'required|alpha_dash|min:6|confirmed',
+                    'password_confirmation' => 'required|same:password',
                 ];
             }
 
             case 'changePassword':
             {
                 return [
-                    'oldPassword'=>'required',
-                    'newPassword'=>'required|alpha_dash|min:6|confirmed',
+                    'oldPassword' => 'required',
+                    'newPassword' => 'required|alpha_dash|min:6|confirmed',
                     'newPassword_confirmation' => 'required|same:newPassword',
                 ];
             }
@@ -71,8 +70,13 @@ class AuthRequest extends FormRequest
     public function messages()
     {
         return [
-            'password_question_id.exists'=>'密保问题不存在',
-            'username.exists'=>'用户不存在'
+            'name.required' => '名称必填',
+            'username.required' => '用户名必填',
+            'username.min' => '用户名长度不小于6位',
+            'password.required' => '密码必填',
+            'password_confirmation.required' => '确认密码必填',
+            'password_question_id.exists' => '密保问题不存在',
+            'username.exists' => '用户不存在'
         ];
     }
 }

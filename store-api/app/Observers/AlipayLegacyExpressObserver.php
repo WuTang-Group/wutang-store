765a96cd36b3f6097daf1c7c851f71709d8dd26a
GabbyMrH <gabbymrh@gmail.com>
2020-09-14 18:12:18 +0800

    AlipayLegacyExpressObserver * 支付宝即时到账观察者增加数据

--- store-api/app/Observers/AlipayLegacyExpressObserver.php
+++ store-api/app/Observers/AlipayLegacyExpressObserver.php
@@ -3,7 +3,9 @@
 namespace App\Observers;
 
 use App\Caches\AlipayLegacyExressCache;
+use App\Enums\PaymentType;
 use App\Models\AlipayLegacyExpress;
+use App\Services\Payment\WebPaymentService;
 
 class AlipayLegacyExpressObserver
 {
@@ -22,35 +24,40 @@ class AlipayLegacyExpressObserver
      */
     public function created(AlipayLegacyExpress $alipayLegacyExpress)
     {
+        if ($alipayLegacyExpress->status == 1) {
+            app(WebPaymentService::class)->store(PaymentType::AlipayLegacyExpress);
+        }
         $this->cache->create($alipayLegacyExpress);
     }
 
     /**
      * Handle the alipay legacy express "updated" event.
      *
-     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @param AlipayLegacyExpress $alipayLegacyExpress
      * @return void
      */
     public function updated(AlipayLegacyExpress $alipayLegacyExpress)
     {
+        app(WebPaymentService::class)->update($alipayLegacyExpress, PaymentType::AlipayLegacyExpress);
         $this->cache->update($alipayLegacyExpress);
     }
 
     /**
      * Handle the alipay legacy express "deleted" event.
      *
-     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @param AlipayLegacyExpress $alipayLegacyExpress
      * @return void
      */
     public function deleted(AlipayLegacyExpress $alipayLegacyExpress)
     {
+        app(WebPaymentService::class)->destroy(PaymentType::AlipayLegacyExpress);
         $this->cache->delete($alipayLegacyExpress);
     }
 
     /**
      * Handle the alipay legacy express "restored" event.
      *
-     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @param AlipayLegacyExpress $alipayLegacyExpress
      * @return void
      */
     public function restored(AlipayLegacyExpress $alipayLegacyExpress)
@@ -61,7 +68,7 @@ class AlipayLegacyExpressObserver
     /**
      * Handle the alipay legacy express "force deleted" event.
      *
-     * @param  AlipayLegacyExpress  $alipayLegacyExpress
+     * @param AlipayLegacyExpress $alipayLegacyExpress
      * @return void
      */
     public function forceDeleted(AlipayLegacyExpress $alipayLegacyExpress)

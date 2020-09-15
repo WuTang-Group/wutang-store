9171ee3499ab2f9ea3fcf661615f5c98cbb45ac1
GabbyMrH <gabbymrh@gmail.com>
2020-09-15 18:07:53 +0800

    Routes * 整合产品、产品分类、the house相关路由

--- store-api/routes/api/index.php
+++ store-api/routes/api/index.php
@@ -46,23 +46,35 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
         /**
          * Product
          */
-        // 根据产品 slug查询对应类别下的产品详情
-        Route::get('product/{product_slug}', 'ProductController@index')->name('product.index');
         // 所有产品列表
-        Route::get('product', 'ProductController@productQueryList')->name('product.productQueryList');
-        // 根据产品状态获取对应的产品列表
-        Route::get('product_status/{status}', 'ProductController@getStatusProduct')->name('product_status.getStatusProduct');
+        Route::get('products', 'ProductController@getList')->name('products.getList');
+        // 根据产品 slug查询对应类别下的产品详情
+        Route::get('product/{product_slug}', 'ProductController@getDetail')->name('product.getDetail');
+        // 根据分类slug获取对应分类下等产品列表
+        Route::get('products/category/{category_slug}', 'ProductController@getListByCategorySlug')->name('products.category.getListByCategorySlug');
+        // 根据产品状态获取对应的产品列表(如新品等)
+        Route::get('products/status/{status}', 'ProductController@getListByStatus')->name('products.status.getListByStatus');
+        // 创新产品推荐
+        Route::get('products/innovate','ProductController@getListByInnovate')->name('products.innovate.getListByInnovate');
         /**
          * Product category
          */
-        Route::get('product_category', 'ProductController@categoryQueryList')->name('product_category.categoryQueryList');
-        // 根据slug查询对应类别下的产品
-        Route::get('product_category/{category_slug}', 'ProductController@getCategoryProduct')->name('product.getCategoryProduct');
-        // 根据ID列表查询商品
-        //Route::get('product_ids', 'ProductController@productQuery')->name('product.productQuery');
-        // 获取分类故事
-        Route::get('category_story/{category_slug}', 'ProductController@categoryStory')->name('product.categoryStory');
+        Route::get('product_category', 'ProductCategoryController@getList')->name('product_category.getList');
+        Route::get('product_category/story/{category_slug}','ProductCategoryController@getStory')->name('product_category.story.getStory');
+        // 产品分类概述(按分类名称)
+        Route::get('product_category/overview/{name}','ProductCategoryController@getOverview')->name('product_category.overview.getOverview');
+        // 探索推荐(属于分类推荐)
+        Route::get('product_category/explore/{category_slug}','ProductCategoryController@getListByExplore')->name('product_category.explore.getListByExplore');
+        /**
+         * The House
+         */
+        // 分类列表(即概述)
+        Route::get('the_house/category','TheHouseController@getCategoryList')->name('the_house.category.getCategoryList');
+        Route::get('the_house/category/{category_slug}','TheHouseController@getListByCategorySlug')->name('the_house.category.getListByCategorySlug');
+        Route::get('the_house/{the_house_slug}','TheHouseController@getDetailByTheHouseSlug')->name('the_house.getDetailByTheHouseSlug');
+        Route::get('the_house/other/{category_slug}','TheHouseController@getOtherCategoryList')->name('the_house.other.getOtherCategoryList');
     });
+
     /**
      * 需登录的路由组
      */
@@ -120,13 +132,6 @@ Route::middleware('throttle:' . config('api.rate_limits.access'))->group(functio
          * Member code
          */
         Route::post('member_code/bind_parent','UserProfileController@bindParentMemberCode')->name('member_code.bind_parent.bindParentMemberCode');
-        /**
-         * The House
-         */
-        Route::get('the_house/category_list', 'TheHouseController@theHouseCategoryList')->name('the_house.theHouseCategoryList');
-        Route::get('the_house/all', 'TheHouseController@theHouseAll')->name('the_house.theHouseAll');
-        Route::get('the_house/list/{category_slug}', 'TheHouseController@theHouseList')->name('the_house.theHouseList');
-        Route::get('the_house/detail/{the_house_slug}', 'TheHouseController@theHouseDetail')->name('the_house.theHouseDetail');
     });
 
 });

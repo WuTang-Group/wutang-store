6dfdb5c9dafcaa891cecd7f505fb60fa4964e832
peng0228 <770601900@qq.com>
2020-09-16 19:10:59 +0800

    * TheHouseContent:TheHouseContent model增加theHouse表的关联关系

--- store-api/app/Models/TheHouseContent.php
+++ store-api/app/Models/TheHouseContent.php
@@ -7,4 +7,9 @@ class TheHouseContent extends Model
     protected $fillable = [
         'the_house_id', 'title', 'content', 'img', 'img_desc'
     ];
+
+    public function theHouse()
+    {
+        return $this->belongsTo(TheHouse::class);
+    }
 }

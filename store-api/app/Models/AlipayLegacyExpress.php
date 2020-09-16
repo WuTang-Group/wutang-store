b5eff9372ef9d0a558a220f5d36b58a014de4217
GabbyMrH <gabbymrh@gmail.com>
2020-09-16 10:41:26 +0800

    AlipayLegacyExpress * 增加允许payment_id写入

--- store-api/app/Models/AlipayLegacyExpress.php
+++ store-api/app/Models/AlipayLegacyExpress.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class AlipayLegacyExpress extends Model
 {
     protected $fillable = [
-        'pid','key','seller_email','return_url','notify_url','status'
+        'pid','key','seller_email','return_url','notify_url','status','payment_id'
     ];
     // 对查询对key值进行加密
     public function getKeyAttribute()

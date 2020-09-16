9799858f2aa687293f8c30cc19073c3e8eb3ab13
GabbyMrH <gabbymrh@gmail.com>
2020-09-16 16:04:38 +0800

    支付类表 * 移除外键约束(便于灵活操作数据)

--- store-api/database/migrations/2020_09_01_165114_create_alipay_aop_pages_table.php
+++ store-api/database/migrations/2020_09_01_165114_create_alipay_aop_pages_table.php
@@ -16,7 +16,7 @@ class CreateAlipayAopPagesTable extends Migration
         Schema::create('alipay_aop_pages', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('payment_id')->nullable()->comment('关联从属payments表id');
-            $table->foreign('payment_id')->references('id')->on('payments');
+//            $table->foreign('payment_id')->references('id')->on('payments')->onUpdate('cascade');
             $table->string('return_url')->comment('前端回调URL');
             $table->string('notify_url')->comment('异步回调URL');
             $table->integer('status')->default(1)->comment('激活状态:1激活，-1未激活');

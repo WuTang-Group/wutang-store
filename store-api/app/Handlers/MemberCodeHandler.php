618097d7ba596f6f2fc86e87961067fc84659a14
GabbyMrH <gabbymrh@gmail.com>
2020-09-17 10:27:47 +0800

    MemberCodeHandler * 重写会员码生成算法

--- store-api/app/Handlers/MemberCodeHandler.php
+++ store-api/app/Handlers/MemberCodeHandler.php
@@ -8,9 +8,18 @@ use Illuminate\Support\Str;
 
 class MemberCodeHandler
 {
-    // 生成会员码
+    /**
+     * 生成会员码
+     * @param int $length 长度
+     * @return string 会员码
+     */
     public static function generateCode($length = 8)
     {
-        return strtoupper(Str::random($length));
+        //return strtoupper(Str::random($length));
+        $randomStr = str_split(strtoupper(Str::random($length)));
+        for ($i = 0; $i < count($randomStr); $i++) {
+            $randomStr[$i] = is_numeric($randomStr[$i]) ? chr(mt_rand(65, 90)) : $randomStr[$i];
+        }
+        return implode('', $randomStr);
     }
 }

fa2e810ad40bbaa45dca4c5a1c468a64a62e80fe
GabbyMrH <gabbymrh@gmail.com>
2020-09-17 17:05:48 +0800

    SocketMode * 增加客户端枚举类

--- store-api/app/Enums/SocketMode.php
+++ store-api/app/Enums/SocketMode.php
@@ -17,11 +17,13 @@ final class SocketMode extends Enum
     /**
      * service
      */
-    const ServerHeartCheck = 'server_heart_check';  // 心跳检测
+    const ServerHeartCheck = 'server_heart_check';  // 服务端心跳检测
     const ServerOrderNotify = 'server_order_notify';  // 服务端订单通知
-    const ServerCommonMessage = 'server_common_message';  // 普通消息
+    const ServerCommonMessage = 'server_common_message';  // 服务端普通消息
 
     /**
      * client
      */
+    const ClientHeartCheck = 'client_heart_check'; // 客户端心跳检测
+    const ClientOrderNotify = 'client_order_notify'; // 客户端订单通知
 }

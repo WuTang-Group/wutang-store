6e4dfa3ab52b24637c51f842f5646e730182c36e
peng0228 <770601900@qq.com>
2020-09-18 18:00:53 +0800

    * theHouseCategory: theHouseCategory服务层

--- store-api/app/Services/Admin/TheHouseCategoryService.php
+++ store-api/app/Services/Admin/TheHouseCategoryService.php
@@ -3,6 +3,7 @@ namespace App\Services\Admin;
 
 use App\Models\TheHouseCategory;
 use App\Services\Service;
+use Illuminate\Support\Facades\Log;
 
 class TheHouseCategoryService extends Service
 {
@@ -12,9 +13,76 @@ class TheHouseCategoryService extends Service
         $this->theHouseCategory = $theHouseCategory;
     }
 
+    // 获取分类列表
     public function getTheHouseCategoryList(array $param)
     {
         $requestData = page_limit($param);
         return $this->theHouseCategory->latest()->paginate($requestData['page_limit']);
     }
+
+     // 获取详情
+    public function getCategoryDetailBySlug(string $slug)
+    {
+        return $this->theHouseCategory->whereSlug($slug)->first();
+    }
+
+    // 创建
+    public function categoryStore(array $params)
+    {
+        $requestData = saveOss($params, ['banner']);
+        try{
+            $this->theHouseCategory->create($requestData);
+        } catch(\Exception $e){
+            Log::error( '创建失败', ['message' => $e->getMessage()]);
+            return false;
+        }
+        return true;
+    }
+
+    // 更新
+    public function categoryUpdateBySlug(string $slug, array $params)
+    {
+        $requestData = saveOss($params, ['banner']);
+        try{
+            $this->theHouseCategory->whereSlug($slug)->update($requestData);
+        } catch(\Exception $e) {
+            Log::error( '更新失败', ['message' => $e->getMessage()]);
+            return false;
+        }
+        return true;
+    }
+
+    // 删除
+    public function categoryDestroyBySlug(string $slug)
+    {
+        try{
+            $this->theHouseCategory->whereSlug($slug)->delete();
+        } catch(\Exception $e) {
+            Log::error( '删除失败', ['message' => $e->getMessage()]);
+            return false;
+        }
+        return true;
+    }
+
+    // 保存静态资源到aliyun OSS
+//    public static function saveOss(array $array)
+//    {
+//        // 保存文件到OSS中，返回url
+//        $filtered = Arr::where($array, function ($value, $key) {
+//            return Str::contains($key, ['banner']) && $key;
+//        });
+//        foreach ($filtered as $key => $value) {
+//            // 图片存储到OSS，本地保存OSS地址
+//            if (is_object($array[$key])) {
+//                try {
+//                    $ossRes = OssHandler::save($array[$key], AliyunOssDir::Product);  // 图片存储到OSS
+//                    $ossRes ? $array[$key] = $ossRes['data'] : null;
+//                } catch (\Exception $e) {
+//                    Log::error($e->getMessage());
+//                    $array[$key] = null;
+//                }
+//            }
+//        }
+//        return $array;
+//    }
 }

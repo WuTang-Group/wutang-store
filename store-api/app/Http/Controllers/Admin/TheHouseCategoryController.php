1426ace3e5af01d196dc20d496cf46c11e94679a
peng0228 <770601900@qq.com>
2020-09-18 18:00:41 +0800

    * theHouseCategory: theHouseCategory控制器层

--- store-api/app/Http/Controllers/Admin/TheHouseCategoryController.php
+++ store-api/app/Http/Controllers/Admin/TheHouseCategoryController.php
@@ -6,6 +6,7 @@ use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Services\Admin\theHouseCategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TheHouseCategoryController extends Controller
 {
@@ -21,4 +22,31 @@ class TheHouseCategoryController extends Controller
         return response()->json(ResponseData::requestSuccess($result));
     }
 
+    public function getCategoryDetailBySlug($slug)
+    {
+        $result = $this->service->getCategoryDetailBySlug($slug);
+        return response()->json(ResponseData::requestSuccess($result));
+    }
+
+    public function categoryStore(Request $request)
+    {
+        $requestData = $request->only(['name', 'banner', 'title', 'sub_title']);
+        $result = $this->service->CategoryStore($requestData);
+        return $result ? response()->json(ResponseData::requestSuccess()) : response()->json(ResponseData::paramError($request->all()));
+    }
+
+    public function CategoryUpdateBySlug($slug, Request $request)
+    {
+        $requestData = $request->only(['name', 'banner', 'title', 'sub_title']);
+        $result = $this->service->categoryUpdateBySlug($slug, $requestData);
+        return $result ? response()->json(ResponseData::requestSuccess()) : response()->json(ResponseData::paramError($request->all()));
+
+    }
+
+    public function categoryDestroyBySlug($slug)
+    {
+        $result = $this->service->categoryDestroyBySlug($slug);
+        return $result ? response()->json(ResponseData::requestSuccess()) : response()->json(ResponseData::paramError());
+
+    }
 }

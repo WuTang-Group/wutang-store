797c5e9ebfe3222b5519a7511068f0fe531c88f3
peng0228 <770601900@qq.com>
2020-09-18 17:59:07 +0800

    * theHouse: theHouse相关接口请求函数

--- store-admin/src/api/theHouse.js
+++ store-admin/src/api/theHouse.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // ======================The House================
 export function theHouseList(param) {
   return request({
-    url: 'admin/the_house/list',
+    url: 'admin/the_houses',
     method: 'get',
     params: param
   })
@@ -11,14 +11,14 @@ export function theHouseList(param) {
 
 export function theHouseDetail(slug) {
   return request({
-    url: '/admin/the_house/detail/' + slug,
+    url: '/admin/the_house/' + slug,
     method: 'get'
   })
 }
 
 export function theHouseStore(data) {
   return request({
-    url: '/admin/the_house/store',
+    url: '/admin/the_house',
     method: 'post',
     data
   })
@@ -26,15 +26,15 @@ export function theHouseStore(data) {
 
 export function theHouseUpdate(slug, data) {
   return request({
-    url: '/admin/the_house/update/' + slug,
-    method: 'put',
+    url: '/admin/the_house/' + slug,
+    method: 'post',
     data
   })
 }
 
 export function theHouseDestroy(slug) {
   return request({
-    url: '/admin/the_house/destroy/' + slug,
+    url: '/admin/the_house/' + slug,
     method: 'delete'
   })
 }
@@ -42,17 +42,84 @@ export function theHouseDestroy(slug) {
 // ======================The House Category================
 export function theHouseCategoryList(param) {
   return request({
-    url: '/admin/the_house/category_list',
+    url: '/admin/the_house/category/list',
     method: 'get',
     params: param
   })
 }
 
-// ======================The House Category================
+export function theHouseCategoryDetail(slug) {
+  return request({
+    url: '/admin/the_house/category/' + slug,
+    method: 'get'
+  })
+}
+
+export function theHouseCategoryStore(data) {
+  return request({
+    url: '/admin/the_house/category',
+    method: 'post',
+    data
+  })
+}
+
+export function theHouseCategoryUpdate(slug, data) {
+  return request({
+    url: '/admin/the_house/category/' + slug,
+    method: 'post',
+    data
+  })
+}
+
+export function theHouseCategoryDelete(slug) {
+  return request({
+    url: '/admin/the_house/category/' + slug,
+    method: 'delete'
+  })
+}
+
+// ======================The House Content================
 export function theHouseContentList(param) {
   return request({
-    url: '/admin/the_house/content_list',
+    url: '/admin/the_house_content/list',
     method: 'get',
     params: param
+  })
+}
+
+export function contentDetail(id) {
+  return request({
+    url: '/admin/the_house_content/' + id,
+    method: 'get'
+  })
+}
+
+export function contentStore(data) {
+  return request({
+    url: '/admin/the_house_content',
+    method: 'post',
+    data
+  })
+}
+
+export function contentUpdate(id, data) {
+  return request({
+    url: '/admin/the_house_content/' + id,
+    method: 'post',
+    data
+  })
+}
+
+export function contentDelete(id) {
+  return request({
+    url: '/admin/the_house_content/' + id,
+    method: 'delete'
+  })
+}
+
+export function contentToTHeHouseList() {
+  return request({
+    url: '/admin/the_houses',
+    method: 'get'
   })
 }

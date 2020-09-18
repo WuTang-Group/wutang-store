72035db95453098c05e717c1fb56cf731efd3e5f
peng0228 <770601900@qq.com>
2020-09-18 18:05:12 +0800

    * payment: payment服务层

---
+++ store-api/app/Services/Admin/Payment/paymentService.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Services\Admin\Payment;
+
+use App\Models\Payment;
+use App\Services\Service;
+use Illuminate\Support\Facades\Log;
+
+class paymentService extends Service
+{
+    protected $payment;
+    public function __construct(Payment $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    public function getPayments($params)
+    {
+        $requestData = page_limit($params);
+        return $this->payment->latest()->paginate($requestData['page_limit']);
+    }
+
+    public function paymentImgStoreById($id, $params)
+    {
+        $requestData = saveOss($params, ['img']);
+        try{
+            $this->payment->find($id)->update($requestData);
+        } catch(\Exception $e) {
+            Log::error('保存支付Logo失败',['message' => $e->getMessage()]);
+            return false;
+        }
+        return true;
+    }
+}

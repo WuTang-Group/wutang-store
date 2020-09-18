ca8ac2d14ecd631ba83c0e24c4c63266d097c5e5
peng0228 <770601900@qq.com>
2020-09-18 18:01:17 +0800

    * theHouseContent: theHouseContent控制器层

--- store-api/app/Http/Controllers/Admin/TheHouseContentController.php
+++ store-api/app/Http/Controllers/Admin/TheHouseContentController.php
@@ -6,6 +6,7 @@ use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Services\Admin\theHouseContentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TheHouseContentController extends Controller
 {
@@ -19,5 +20,32 @@ class TheHouseContentController extends Controller
     {
         $result = $this->service->getTheHouseContentList($request->all());
         return response()->json(ResponseData::requestSuccess($result));
+    }
+
+    public function getContentDetailById($id)
+    {
+        $result = $this->service->getContentDetailById($id);
+        return response()->json(ResponseData::requestSuccess($result));
+    }
+
+    public function contentStore(Request $request)
+    {
+        Log::info($request->all());
+        $requestData = $request->only(['id', 'the_house_id', 'title', 'content', 'img', 'img_desc']);
+        $result = $this->service->contentStore($requestData);
+        return $result? response()->json(ResponseData::requestSuccess()): response()->json(ResponseData::paramError($request->all()));
+    }
+
+    public function ContentUpdateById($id, Request $request)
+    {
+        $requestData = $request->only(['id', 'the_house_id', 'title', 'content', 'img', 'img_desc']);
+        $result = $this->service->ContentUpdateById($id, $requestData);
+        return $result? response()->json(ResponseData::requestSuccess()):response()->json(ResponseData::paramError($request->all()));
+    }
+
+    public function contentDestroyById($id)
+    {
+        $result = $this->service->contentDestroyById($id);
+        return $result? response()->json(ResponseData::requestSuccess()):response()->json(ResponseData::paramError());
     }
 }

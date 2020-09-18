538f4e380a9493dae4951fc9dec68ba142aca317
GabbyMrH <gabbymrh@gmail.com>
2020-09-18 14:23:22 +0800

    Service * 新增首页服务类

---
+++ store-api/app/Services/Api/HomeService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Enums\AssetImgLocation;
+use App\Enums\AssetImgType;
+use App\Models\AssetImg;
+use App\Models\TheHouse;
+use App\Services\Service;
+
+class HomeService extends Service
+{
+    private $assetImg, $theHouse;
+
+    public function __construct(AssetImg $assetImg, TheHouse $theHouse)
+    {
+        $this->assetImg = $assetImg;
+        $this->theHouse = $theHouse;
+    }
+
+    // 获取首页数据信息
+    public function getList()
+    {
+        return [
+            'banner' => $this->getBanner(),
+            'discover' => $this->getDiscoverList(),
+            'the_house' => $this->getTheHouseList()
+        ];
+    }
+
+    // 获取首页banner数据
+    public function getBanner()
+    {
+        return $this->assetImg->whereType(AssetImgType::Banner)->whereImgLocation(AssetImgLocation::Index)
+            ->with(['product' => function ($query) {
+                $query->select('id', 'slug');
+            }])->first();
+    }
+
+    // 获取首页discover列表数据
+    public function getDiscoverList()
+    {
+        return $this->assetImg->whereType(AssetImgType::Discover)->whereImgLocation(AssetImgLocation::Index)
+                ->with(['product'=>function($query){
+                    $query->select('id','slug');
+                }])->get();
+    }
+
+    // 获取the house 列表数据
+    public function getTheHouseList()
+    {
+        return $this->theHouse->inRandomOrder()->take(3)->get();
+    }
+
+}

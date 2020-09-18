da489cf2c2e7a793e7538b497482670a8bb930f7
peng0228 <770601900@qq.com>
2020-09-18 17:56:03 +0800

    * payments： 接口增加payments相关路由

--- store-api/routes/admin/payment.php
+++ store-api/routes/admin/payment.php
@@ -8,3 +8,6 @@ Route::post('alipay/legacy_express','Payment\AlipayLegacyExpressController@store
 Route::put('alipay/legacy_express/{id}','Payment\AlipayLegacyExpressController@update')->name('alipay.legacy_express.update');
 Route::delete('alipay/legacy_express/{id}','Payment\AlipayLegacyExpressController@delete')->name('alipay.legacy_express.delete');
 Route::put('alipay/legacy_express/status/{id}','Payment\AlipayLegacyExpressController@updateStatus')->name('alipay.legacy_express.status.updateStatus');
+
+Route::get('payment', 'Payment\paymentController@getPayments')->name('payment');
+Route::post('payment/{id}', 'Payment\paymentController@paymentImgStoreById')->name('payment');

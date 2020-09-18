f05f8d44fc28d09e4a1bfed93db6a18bb1cb8534
GabbyMrH <gabbymrh@gmail.com>
2020-09-18 13:45:55 +0800

    AssetImgFactory * 新增静态资源管理模型数据类

---
+++ store-api/database/factories/AssetImgFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\AssetImg::class, function (Faker $faker) {
+    return [
+        'img' => $faker->imageUrl(),
+        'img_location' => $faker->numberBetween(1,2),
+        'type' => $faker->randomElement(['banner','discover']),
+        'product_id' => $faker->numberBetween(1,10),
+        'created_at' => $faker->dateTimeThisMonth(),
+        'updated_at' => $faker->dateTimeThisMonth()
+    ];
+});

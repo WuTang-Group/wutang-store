de12f22cdbda6778c634f41d1d9841fed8b22baa
peng0228 <770601900@qq.com>
2020-09-18 18:06:07 +0800

    * observer: TheHouseCategory观察者

---
+++ store-api/app/Observers/TheHouseCategoryObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Handlers\SlugTranslateHandler;
+use App\Http\Controllers\Admin\TheHouseCategoryController;
+use App\Models\TheHouseCategory;
+
+class TheHouseCategoryObserver
+{
+    public function saving(TheHouseCategory $theHouseCategory)
+    {
+        $theHouseCategory->sub_title = clean($theHouseCategory->sub_title, 'sub_title');
+        if (!$theHouseCategory->slug) {
+            $theHouseCategory->slug = app(SlugTranslateHandler::class)->translate($theHouseCategory->sub_title);
+        }
+    }
+}

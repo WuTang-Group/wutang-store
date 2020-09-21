7e2e08dde36512575ac0d39a4a3c3ab45e9c4943
peng0228 <770601900@qq.com>
2020-09-21 16:35:57 +0800

    * 调整命名,assetImgs修改未assetImg * 增加type、product_id字段处理

--- store-api/app/Http/Controllers/Admin/AssetImgController.php
+++ store-api/app/Http/Controllers/Admin/AssetImgController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AssetImgsRequest;
-use App\Services\Admin\AssetImgsService;
+use App\Services\Admin\AssetImgService;
 use Illuminate\Http\Request;
 
 /**
@@ -14,16 +14,16 @@ use Illuminate\Http\Request;
  * @authenticated
  * @package App\Http\Controllers\Admin
  */
-class AssetImgsController extends Controller
+class AssetImgController extends Controller
 {
-    private $assetImgsService;
-    public function __construct(AssetImgsService $assetImgsService)
+    private $assetImgService;
+    public function __construct(AssetImgService $assetImgService)
     {
-        $this->assetImgsService = $assetImgsService;
+        $this->assetImgService = $assetImgService;
     }
 
     /**
-     * Get assetImgs list
+     * Get assetImg list
      * 获取首页静态资源列表
      * @queryParam page 页码
      * @queryParam page_limit 每页数量
@@ -31,33 +31,37 @@ class AssetImgsController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function queryList(Request $request){
-        $result = $this->assetImgsService->queryList($request->all());
+        $result = $this->assetImgService->queryList($request->all());
         return response()->json(ResponseData::requestSuccess($result));
     }
 
     /**
-     * save asset img
+     * Save asset img
      * 保存首页静态资源
-     * @queryParam img required 图片地址
-     * @queryParam img_location required 图片位置
+     * @bodyParam img required 图片地址
+     * @bodyParam img_location required 图片位置
+     * @bodyParam type required 类型
+     * @bodyParam product_id 关联产品
      * @param AssetImgsRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
     public function store(AssetImgsRequest $request)
     {
-        $result = $this->assetImgsService->store(array_filter($request->all()));
+        $requestData = $request->only(['img', 'img_location', 'type', 'product_id']);
+        $result = $this->assetImgService->store(array_filter($requestData));
         return $result? response()->json(ResponseData::requestSuccess($request)): response()->json(ResponseData::requestFails($request->all()));
     }
 
     /**
      * Delete img
      * 删除图片资源
+     * @urlParam assetImgId required Example:1
      * @param $assetImgId
      * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($assetImgId)
     {
-        $result = $this->assetImgsService->destroy($assetImgId);
+        $result = $this->assetImgService->destroy($assetImgId);
         return $result? response()->json(ResponseData::requestSuccess()): response()->json(ResponseData::paramError());
     }
 }

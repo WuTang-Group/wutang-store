56c9671a149021035ff7ba03195d9f81e88fc26e
GabbyMrH <gabbymrh@gmail.com>
2020-09-29 15:04:11 +0800

    Roles.php * 新增编辑者枚举类

--- store-api/app/Enums/Roles.php
+++ store-api/app/Enums/Roles.php
@@ -13,4 +13,5 @@ final class Roles extends Enum
     const Admin =   'admin';  // 管理员
     const Customer = 'customer'; // 普通用户
     const Visitor = 'visitor'; // 访客用户
+    const Editor = 'editor';  // 维护人员-网站后台文案和图片维护
 }

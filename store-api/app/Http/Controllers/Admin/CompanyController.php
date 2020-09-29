620c64a278d13cfcbaeaa8faf84be6724c18c85c
GabbyMrH <gabbymrh@gmail.com>
2020-09-29 15:00:43 +0800

    CompanyController * 增加公司列表 * 增加公司下部门列表

--- store-api/app/Http/Controllers/Admin/CompanyController.php
+++ store-api/app/Http/Controllers/Admin/CompanyController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Services\Admin\CompanyService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+/**
+ * @group [ADMIN] Company manage
+ * @authenticated
+ * 公司管理
+ * @package App\Http\Controllers\Admin
+ */
 class CompanyController extends Controller
 {
     private $service;
@@ -15,9 +25,31 @@ class CompanyController extends Controller
         $this->service = $service;
     }
 
-    public function index()
+    /**
+     * Get company list
+     * 获取公司列表
+     * @queryParam page 页码
+     * @queryParam page_limit 显示条目
+     * @param Request $request
+     * @return Application|ResponseFactory|Response
+     */
+    public function getList(Request $request)
     {
+        $companies = $this->service->getList($request);
+        return response(ResponseData::requestSuccess($companies));
+    }
 
+    /**
+     * Get depatments by company id
+     * 根据公司ID获取下辖部门列表
+     * @urlParam company_id 公司id
+     * @param $company_name
+     * @return Application|ResponseFactory|Response
+     */
+    public function getDepartmentListByCompany($company_name)
+    {
+        $departmentCollection = $this->service->getDepartmentListByCompany($company_name);
+        return response(ResponseData::requestSuccess($departmentCollection));
     }
 
 }

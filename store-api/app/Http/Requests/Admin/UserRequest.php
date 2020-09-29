e4f06e8c8fd97f99612f1c3740d4f9e33e2317b4
GabbyMrH <gabbymrh@gmail.com>
2020-09-29 15:05:29 +0800

    UserRequest.php * 用户表单验证器增加写入方法验证

--- store-api/app/Http/Requests/Admin/UserRequest.php
+++ store-api/app/Http/Requests/Admin/UserRequest.php
@@ -2,6 +2,7 @@
 namespace App\Http\Requests\Admin;
 
 use App\Http\Requests\FormRequest;
+use App\Models\User;
 
 
 class UserRequest extends FormRequest
@@ -10,7 +11,7 @@ class UserRequest extends FormRequest
     {
         switch ($this->route()->getActionMethod())
         {
-            case 'password':
+            case 'resetPassword':
             {
                 return [
                     'oldPassword'=>'required',
@@ -24,7 +25,36 @@ class UserRequest extends FormRequest
                     'hash_id'=>'required'
                 ];
             }
+            case 'store':
+            {
+                return [
+                    'username' => ['required',function($attribute, $value, $fail){
+                        $username = User::whereUsername($value)->first();
+                        if($username){
+                            return $fail('用户名已存在');
+                        }
+                    }],
+                    'password' => 'required|min:6',
+                    'name' => 'required',
+                    'role' => 'required',
+                    'company' => 'required',
+                    'department' => 'required'
+                ];
+            }
         }
+    }
+
+    public function messages()
+    {
+        return [
+            'username.required' => '用户名必填',
+            'password.required' => '密码必填',
+            'password.min' => '密码长度不符合规范',
+            'name.required' => '姓名必填',
+            'role.required' => '角色必选',
+            'company.required' => '公司必选',
+            'department.required' => '部门必选'
+        ];
     }
 
 }

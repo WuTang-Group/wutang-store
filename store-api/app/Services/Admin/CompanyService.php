7dc3ab57ffebd3d04697f0ceefef3cbc6f93bad0
GabbyMrH <gabbymrh@gmail.com>
2020-09-29 15:01:07 +0800

    CompanyService * 增加公司控制器对应服务类方法

--- store-api/app/Services/Admin/CompanyService.php
+++ store-api/app/Services/Admin/CompanyService.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Services\Admin;
 
 use App\Models\Company;
 use App\Services\Service;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CompanyService extends Service
 {
@@ -11,6 +13,24 @@ class CompanyService extends Service
     public function __construct(Company $company)
     {
         $this->company = $company;
+    }
+
+    // 公司列表
+    public function getList(object $params)
+    {
+        $requestData = page_limit($params->all());
+        // distinct()与laest()同用会无法去重
+        return $this->company->distinct()->paginate($requestData['page_limit']);
+    }
+
+    // 根据公司获取该公司下的部门列表
+    public function getDepartmentListByCompany(string $companyName)
+    {
+        try {
+            return $this->company->whereName($companyName)->with(['departments'])->firstOrFail()->departments;
+        } catch (ModelNotFoundException $e) {
+            return [];
+        }
     }
 
 

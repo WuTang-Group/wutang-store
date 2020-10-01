b0a970b88e264895a07a1191d8d77a01d340bb94
GabbyMrH <gabbymrh@gmail.com>
2020-10-01 14:00:12 +0800

    ContactUsRequest * 新增联系我们表单验证器

---
+++ store-api/app/Http/Requests/Api/ContactUsRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Http\Requests\FormRequest;
+
+class ContactUsRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        switch ($this->route()->getActionMethod())
+        {
+            case 'store':
+            {
+                return [
+                    'name' => 'required',
+                    'type' => 'required',
+                    'content' => 'required'
+                ];
+            }
+        }
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '姓名必填',
+            'type.required' => '类型必选',
+            'content.required' => '内容必填'
+        ];
+    }
+}

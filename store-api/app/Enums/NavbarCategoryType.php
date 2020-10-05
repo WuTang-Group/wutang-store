fe74b56fb1edb29f908fddf3a36e8b82e202d895
GabbyMrH <932585440@qq.com>
2020-10-05 15:49:09 +0800

    Enums * 增加导航分类类型枚举数据类

---
+++ store-api/app/Enums/NavbarCategoryType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+
+final class NavbarCategoryType extends Enum
+{
+    const Product = 'product';  // 产品系列
+    const ProductCategorySkinCare = 'product_category_skin_care';  // 产品分类
+}

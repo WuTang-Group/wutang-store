c0d3c99a72dcc372cdc7699ce41a1741cbcfa466
GabbyMrH <932585440@qq.com>
2020-10-05 15:54:27 +0800

    web * 更新产品和产品类路由

--- store-api/routes/api/web.php
+++ store-api/routes/api/web.php
@@ -9,23 +9,23 @@ Route::get('home','HomeController@getList')->name('home.getList');
  */
 // 所有产品列表
 Route::get('products', 'ProductController@getList')->name('products.getList');
-// 根据产品 slug查询对应类别下的产品详情
+// 根据产品 slug查询产品详情
 Route::get('product/{product_slug}', 'ProductController@getDetail')->name('product.getDetail');
 // 根据分类slug获取对应分类下等产品列表
-Route::get('products/category/{category_slug}', 'ProductController@getListByCategorySlug')->name('products.category.getListByCategorySlug');
+Route::get('products/list/{navbar_category_type}/{slug}', 'ProductController@getListBySlug')->name('products.list.getListBySlug');
 // 根据产品状态获取对应的产品列表(如新品等)
 Route::get('products/status/{status}', 'ProductController@getListByStatus')->name('products.status.getListByStatus');
 // 创新产品推荐
 Route::get('products/innovate','ProductController@getListByInnovate')->name('products.innovate.getListByInnovate');
+// 继续探索推荐(属于产品系列)
+Route::get('products/explore','ProductController@getListByExplore')->name('products.explore.getListByExplore');
 /**
  * Product category
  */
 Route::get('product_category', 'ProductCategoryController@getList')->name('product_category.getList');
 Route::get('product_category/story/{category_slug}','ProductCategoryController@getStory')->name('product_category.story.getStory');
-// 产品分类概述(按分类名称)
-Route::get('product_category/overview/{categpory_slug}','ProductCategoryController@getOverview')->name('product_category.overview.getOverview');
-// 探索推荐(属于分类推荐)
-Route::get('product_category/explore/{category_slug}','ProductCategoryController@getListByExplore')->name('product_category.explore.getListByExplore');
+// 导航产品分类概述(按分类名称)
+Route::get('product_category/overview/{navbar_category_type}','ProductCategoryController@getOverview')->name('product_category.overview.getOverview');
 /**
  * The House
  */

ed7f656bb443e18fccaf44488a3d08900455de26
GabbyMrH <932585440@qq.com>
2020-10-05 15:48:41 +0800

    HomeService * 增加首页返回当月推荐数据

--- store-api/app/Services/Api/HomeService.php
+++ store-api/app/Services/Api/HomeService.php
@@ -24,6 +24,7 @@ class HomeService extends Service
         return [
             'banner' => $this->getBanner(),
             'discover' => $this->getDiscoverList(),
+            'recommend_list' => $this->getRecommendListForThisMonth(),
             'the_house' => $this->getTheHouseList()
         ];
     }
@@ -31,25 +32,42 @@ class HomeService extends Service
     // 获取首页banner数据
     public function getBanner()
     {
-        return $this->assetImg->whereType(AssetImgType::Banner)->whereImgLocation(AssetImgLocation::Index)
-            ->with(['product' => function ($query) {
-                $query->select('id', 'slug','product_name','product_name_en');
-            }])->first()->makeHidden(['created_at','updated_at']);
+        try {
+            return $this->assetImg->whereType(AssetImgType::Banner)->whereImgLocation(AssetImgLocation::Index)
+                ->with(['product' => function ($query) {
+                    $query->select('id', 'slug', 'product_name', 'product_name_en');
+                }])->firstOrFail()->makeHidden(['created_at', 'updated_at']);
+        } catch (\Exception $e) {
+            return '';
+        }
+    }
+
+    // 获取首页当月推荐数据
+    public function getRecommendListForThisMonth()
+    {
+        try {
+            return $this->assetImg->whereType(AssetImgType::ThisMonthRecommannd)->whereImgLocation(AssetImgLocation::Index)
+                ->with(['product' => function ($query) {
+                    $query->select('id', 'slug', 'product_name', 'product_name_en');
+                }])->firstOrFail()->makeHidden(['created_at', 'updated_at']);
+        } catch (\Exception $e) {
+            return [];
+        }
     }
 
     // 获取首页discover列表数据
     public function getDiscoverList()
     {
         return $this->assetImg->whereType(AssetImgType::Discover)->whereImgLocation(AssetImgLocation::Index)
-                ->with(['product'=>function($query){
-                    $query->select('id','slug','product_name','product_name_en');
-                }])->get()->makeHidden(['created_at','updated_at']);
+            ->with(['product' => function ($query) {
+                $query->select('id', 'slug', 'product_name', 'product_name_en');
+            }])->get()->makeHidden(['created_at', 'updated_at']);
     }
 
     // 获取the house 列表数据
     public function getTheHouseList()
     {
-        return $this->theHouse->inRandomOrder()->take(3)->get()->makeHidden(['created_at','updated_at']);
+        return $this->theHouse->inRandomOrder()->take(3)->get()->makeHidden(['created_at', 'updated_at']);
     }
 
 }

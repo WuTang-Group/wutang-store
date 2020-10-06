ead84e1f973ee45b5a45192f4b20c15d2600f746
GabbyMrH <gabbymrh@gmail.com>
2020-10-06 12:48:26 +0800

    Products * 产品分类模型和产品模型图片路径更改

--- store-api/database/factories/ProductCategoriesFactory.php
+++ store-api/database/factories/ProductCategoriesFactory.php
@@ -8,14 +8,14 @@ $factory->define(\App\Models\ProductCategory::class, function (Faker $faker) {
     return [
         'name'=>$faker->randomElement(['面膜', '洁面', '爽肤水', '眼霜', '面霜', '眼线', '粉底液', 'BB霜', '隔离', '睫毛膏', '彩妆盘', '唇膏', '腮红', '香水', '精油', '身体护理']),
         'slug' => \Str::slug($faker->words(2, true)),
-        'thumbnail' => \App\Handlers\RandomImageHandler::generateImage()->img_url,
-        'banner'=>\App\Handlers\RandomImageHandler::generateImage()->img_url,
+        'thumbnail' => $faker->imageUrl(),
+        'banner'=>$faker->imageUrl(),
         'title' => $faker->words(3, true),
         'title_en'=>$faker->words(3, true),
         'sub_title' => $faker->words(3, true),
         'describe'=>$faker->text ,
         'describe_en'=>$faker->text ,
-        'describe_img' => \App\Handlers\RandomImageHandler::generateImage()->img_url,
+        'describe_img' => $faker->imageUrl(),
         'parent_id'=>$faker->numberBetween(0, 30),
         'created_at' => $faker->dateTimeThisMonth(),
         'updated_at' => $faker->dateTimeThisMonth()

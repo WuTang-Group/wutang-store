5a90c820d22309f7ae61948d1d808e8ae5348873
GabbyMrH <gabbymrh@gmail.com>
2020-10-06 14:29:40 +0800

    AssetImgFactory * 静态资源模型更新

--- store-api/database/factories/AssetImgFactory.php
+++ store-api/database/factories/AssetImgFactory.php
@@ -8,7 +8,7 @@ $factory->define(\App\Models\AssetImg::class, function (Faker $faker) {
     return [
         'img' => $faker->imageUrl(),
         'img_location' => $faker->numberBetween(1,2),
-        'type' => $faker->randomElement(['banner','discover']),
+        'type' => $faker->randomElement(['banner','discover','this_month_recommand']),
         'product_id' => $faker->numberBetween(1,10),
         'created_at' => $faker->dateTimeThisMonth(),
         'updated_at' => $faker->dateTimeThisMonth()

312176b29b389dc777a0a10b76c51c33003fe472
GabbyMrH <gabbymrh@gmail.com>
2020-10-07 16:46:55 +0800

    ResponseStatusCode.php * 增加商品返回code分组

--- store-api/app/Enums/ResponseStatusCode.php
+++ store-api/app/Enums/ResponseStatusCode.php
@@ -10,7 +10,11 @@ use BenSampo\Enum\Enum;
  */
 final class ResponseStatusCode extends Enum
 {
+    /**
+     * 通用返回code
+     */
     const ParamError =   10001;       //参数有误
+    const DataExist = 10002;         // 数据已存在
     const DataError =   10003;        //数据有误
     const RequestSuccess = 20001;       //操作成功
     const RequestDeny = 40001;          //禁止访问
@@ -18,4 +22,12 @@ final class ResponseStatusCode extends Enum
     const QueryVoid = 40004;          //查询为空
     const TokenExpired = 40005;       //请求过期
     const RequestFails = 50001;         //操作失败
+
+    /**
+     * 商品类返回code
+     */
+    const ProductShopCartExist = 'P1001';  // 产品存在购物车
+    const ProductBought = 'P1002';  // 已购买过该商品
+    const ProductNotExist = 'P4004';  // 产品不存在
+    const ProductSoldOut = 'P4005';   // 产品售罄
 }

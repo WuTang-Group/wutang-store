55010d77b95152b5c43dcf2540e91c9f44767066
GabbyMrH <gabbymrh@gmail.com>
2020-10-08 17:13:26 +0800

    AlipayBankGateway * 支付宝网关支付模型允许写入数据

--- store-api/app/Models/AlipayBankGateway.php
+++ store-api/app/Models/AlipayBankGateway.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class AlipayBankGateway extends Model
 {
     protected $fillable = [
-        ''
+        'payment_id','merch_id','product','key','return_url','notify_url','status'
     ];
 
     /**

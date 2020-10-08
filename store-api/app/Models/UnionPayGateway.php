1f1fe9afa7eeb04bcabc4a7d095d604686eeccb8
GabbyMrH <gabbymrh@gmail.com>
2020-10-08 17:20:31 +0800

    models * 新增银联网关模型

---
+++ store-api/app/Models/UnionPayGateway.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnionPayGateway extends Model
+{
+    protected $fillable = [
+        'payment_id', 'cpid', 'pay_type', 'jump_url', 'notify_url','status'
+    ];
+
+    /**
+     * 从属关联Payment模型
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+}

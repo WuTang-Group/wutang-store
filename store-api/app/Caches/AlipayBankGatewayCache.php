f08e947cde4a4be30605f2be95e73354b25588c4
GabbyMrH <gabbymrh@gmail.com>
2020-10-10 13:39:15 +0800

    AlipayBankgatewayCache * 新增支付宝网关支付模型-缓存类

---
+++ store-api/app/Caches/AlipayBankGatewayCache.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Caches;
+
+
+use App\Caches\Contracts\BaseModelCacheInterface;
+use App\Enums\CacheKeyPrefix;
+use App\Models\AlipayBankGateway;
+use Illuminate\Support\Facades\Cache;
+
+class AlipayBankGatewayCache implements BaseModelCacheInterface
+{
+    private $alipayBankGateway;
+
+    public function __construct(AlipayBankGateway $alipayBankGateway)
+    {
+        $this->alipayBankGateway = $alipayBankGateway;
+    }
+
+    public function save($model)
+    {
+        $key = CacheKeyPrefix::AlipayBankGateway . $model->id;
+        if (Cache::has($key)) {
+            Cache::forget($key);
+        }
+        Cache::forever($key, $this->alipayBankGateway->find($model->id));
+    }
+
+    public function delete($model)
+    {
+        $key = CacheKeyPrefix::AlipayBankGateway . $model->id;
+        if (Cache::has($key)) {
+            Cache::forget($key);
+        }
+    }
+}

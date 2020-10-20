65915f3bab9598d9e648e08463132cedc5c2831a
GabbyMrH <gabbymrh@gmail.com>
2020-10-20 17:21:58 +0800

    feat(Controller):会员码控制器临时新增用户资料验证

--- store-api/app/Http/Controllers/Api/MemberCodeController.php
+++ store-api/app/Http/Controllers/Api/MemberCodeController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Handlers\ResponseData;
 use App\Http\Controllers\Controller;
 use App\Services\Api\MemberCodeService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MemberCodeController extends Controller
 {
@@ -16,9 +19,26 @@ class MemberCodeController extends Controller
         $this->service = $service;
     }
 
+    /**
+     * Generate member code for user
+     * 为用户生成会员码(注册后自动生成)
+     * @param Request $request
+     * @return Application|ResponseFactory|Response
+     */
     public function generateMemberCode(Request $request)
     {
         $result = $this->service->generateMemberCode();
         return $result ? response(ResponseData::requestSuccess($result)) : response(ResponseData::requestFails($request->all()));
+    }
+
+    /**
+     * Request check mamber code parent
+     * 请求验证会员码父级
+     * @return Application|ResponseFactory|Response
+     */
+    public function checkCodeParent()
+    {
+        $checkCodeParent = $this->service->checkCodeParent();
+        return $checkCodeParent ? response(ResponseData::requestSuccess($checkCodeParent)) : response(ResponseData::dataError('用户无会员码或未绑定上级'));
     }
 }

2f750de14a421201c45941e1fc698a80aed44753
GabbyMrH <gabbymrh@gmail.com>
2020-10-23 11:36:24 +0800

    refactor(会员码服务-API):重构绑定父级会员码方法

--- store-api/app/Services/Api/MemberCodeService.php
+++ store-api/app/Services/Api/MemberCodeService.php
@@ -34,21 +34,31 @@ class MemberCodeService extends Service
         }
     }
 
-    // 绑定上级会员码
-    public function bindParentCode($params)
+    // 绑定上级会员码(即获取输入会员码的id，取代users表内的member_code_id)
+    // 注意：是绑定用户的上级，不是会员码的上级；即为用户绑定会员码
+    public function bindParentCode(object $params)
     {
         try {
-            // 上级
-            $parent_code = $this->memberCode->whereCode($params->code)->firstOrFail();
-            // 本级
-            $current_code = $this->memberCode->where('user_id', $this->user()->id)->firstOrFail();
-
-            $current_code->parent_id = $parent_code->parent_id;  // 关联上级code
-            $current_code->level = $parent_code->level + 1;  // 等级在上级基础+1
-            $current_code->save();
+            // 若用户未绑定会员码，则先绑定。不允许用户绑定自己的会员码
+            if ($this->user()->member_code_id) {
+                return false;  // 已绑定上级会员码,无法再次绑定
+            }
+            $memberCode = $this->memberCode->whereCode($params->code)->first();
+            if ($this->user()->id == $memberCode->user_id) {
+                return false; // 不允许绑定自身会员码
+            }
+            $this->user()->member_code_id = $memberCode->id;
+            $this->user()->save();
+            // 绑定了上级会员码，本级会员码(若有)则在该上级会员码的level基础上+1
+            $current_user_code = $this->memberCode->whereUserId($this->user()->id)->first();
+            if ($current_user_code) {
+                $current_user_code->level = $memberCode->level + 1;
+                $current_user_code->save();
+            }
+            return $params->code;
         } catch (\Exception $e) {
             Log::error('会员码绑定失败', ['message' => $e->getMessage()]);
-            return false;
+            return $e->getMessage();
         }
     }
 

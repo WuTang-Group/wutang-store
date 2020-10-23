09f45e820699b5aceab2c12873514431a219c77e
GabbyMrH <gabbymrh@gmail.com>
2020-10-23 17:34:32 +0800

    feat(默认数据种子)

--- store-api/database/migrations/2020_07_22_056666_seed_roles_and_permissions_data.php
+++ store-api/database/migrations/2020_07_22_056666_seed_roles_and_permissions_data.php
@@ -42,7 +42,7 @@ class SeedRolesAndPermissionsData extends Migration
             'phone'=>'12312312312',
             'email'=>'admin@admin.com',
             'email_verified_at'=>now(),
-            'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password'=>'$2y$10$q2FjX5JlR/1uYEy4DGtXceNKOIZt1WG32pzrVqKgmTCkZRaJ5qOTS',
             'remember_token' =>\Illuminate\Support\Str::random(15),
             'password_question_id' => 1,
             'password_answer' => 'test',

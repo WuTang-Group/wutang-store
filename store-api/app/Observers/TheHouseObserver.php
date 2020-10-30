9230ee85b04d3c3499e66f22700b31331a39d87f
GabbyMrH <gabbymrh@gmail.com>
2020-10-30 15:55:52 +0800

    fix(Observer):slug自动生成机制调整

--- store-api/app/Observers/TheHouseObserver.php
+++ store-api/app/Observers/TheHouseObserver.php
@@ -4,16 +4,20 @@ namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
 use App\Models\TheHouse;
+use Illuminate\Support\Str;
 
 class TheHouseObserver
 {
     public function saving(TheHouse $theHouse)
     {
         // XSS 过滤
-        $theHouse->describe = clean($theHouse->describe, 'custom');
-
+//        $theHouse->describe = clean($theHouse->describe, 'custom');
+//
+//        if (!$theHouse->slug) {
+//            $theHouse->slug = app(SlugTranslateHandler::class)->translate($theHouse->describe);
+//        }
         if (!$theHouse->slug) {
-            $theHouse->slug = app(SlugTranslateHandler::class)->translate($theHouse->describe);
+            $theHouse->slug = Str::random();
         }
     }
 }
